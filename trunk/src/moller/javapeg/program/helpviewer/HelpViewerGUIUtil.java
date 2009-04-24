b785dbda91a4b151b930fc6d5f45d4983f595cda
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2009-04-24 21:14:10 +0000

    Renamed the Class file and added a method that resolves a selected row number in a JTree to a file on the file system.

--- trunk/src/moller/javapeg/program/helpviewer/HelpViewerGUIUtil.java
+++ trunk/src/moller/javapeg/program/helpviewer/HelpViewerGUIUtil.java
@@ -1,7 +1,7 @@
 package moller.javapeg.program.helpviewer;
 /**
  * This class was created : 2009-04-20 by Fredrik Möller
- * Latest changed         : 
+ * Latest changed         : 2009-04-24 by Fredrik Möller
  */
 
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -9,7 +9,7 @@ import javax.swing.tree.TreeNode;
 
 import moller.javapeg.program.language.Language;
 
-public class HelpViewerGUITreeNode {
+public class HelpViewerGUIUtil {
 	
 	public static TreeNode createNodes() {
 		
@@ -21,9 +21,28 @@ public class HelpViewerGUITreeNode {
 		DefaultMutableTreeNode overViewCreator    = new DefaultMutableTreeNode(lang.get("helpViewerGUI.tree.overviewCreator"));
 		
 		root.add(programHelp);
-		root.add(versionInformation);
 		root.add(overViewCreator);
-
+		root.add(versionInformation);
+		
 		return root;	
+	}
+	
+	public static String getFile(int selectedRow) {
+		String fileToLoad = "";
+		
+		switch (selectedRow) {
+		case 1:
+			fileToLoad = "/user_manual";
+			break;
+		case 2:
+			fileToLoad = "/thumbnail_overview";
+			break;
+		case 3:
+			fileToLoad = "/version_information";
+			break;
+		default:
+			break;
+		}
+		return fileToLoad;
 	}
 }
