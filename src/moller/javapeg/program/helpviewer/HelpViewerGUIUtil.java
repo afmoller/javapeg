a1feeb1af8ee1d79d053ce0dd79ac97a26379d6a
fredrik_m <afmoller@users.noreply.github.com>
2009-04-27 20:27:20 +0000

    Added references item to the tree structure

--- src/moller/javapeg/program/helpviewer/HelpViewerGUIUtil.java
+++ src/moller/javapeg/program/helpviewer/HelpViewerGUIUtil.java
@@ -2,6 +2,7 @@ package moller.javapeg.program.helpviewer;
 /**
  * This class was created : 2009-04-20 by Fredrik Möller
  * Latest changed         : 2009-04-24 by Fredrik Möller
+ *                        : 2009-04-27 by Fredrik Möller
  */
 
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -19,10 +20,12 @@ public class HelpViewerGUIUtil {
 		DefaultMutableTreeNode programHelp        = new DefaultMutableTreeNode(lang.get("helpViewerGUI.tree.programHelp"));
 		DefaultMutableTreeNode versionInformation = new DefaultMutableTreeNode(lang.get("helpViewerGUI.tree.versionInformation"));
 		DefaultMutableTreeNode overViewCreator    = new DefaultMutableTreeNode(lang.get("helpViewerGUI.tree.overviewCreator"));
+		DefaultMutableTreeNode references         = new DefaultMutableTreeNode(lang.get("helpViewerGUI.tree.references"));
 		
 		root.add(programHelp);
 		root.add(overViewCreator);
 		root.add(versionInformation);
+		root.add(references);
 		
 		return root;	
 	}
@@ -39,6 +42,9 @@ public class HelpViewerGUIUtil {
 			break;
 		case 3:
 			fileToLoad = "/version_information";
+			break;
+		case 4:
+			fileToLoad = "/references";
 			break;
 		default:
 			break;
