0489f1b5175590af6526190b240a7f5c876cafa3
fredrik_m <afmoller@users.noreply.github.com>
2009-05-03 21:06:53 +0000

    Helper classes for GridBagLayout

---
+++ src/moller/javapeg/program/Gap.java
@@ -0,0 +1,36 @@
+// File   : layoutDemos/layoutDemoGB/Gap.java
+// Purpose: Gaps for use in GridBagLayout (or any other).
+//          Library alternatives are available in the Box class.
+// Author : Fred Swartz - January 30, 2007 - Placed in public domain.
+
+package moller.javapeg.program;
+/**
+ * This class was created : 2009-05-02 by Fredrik Möller
+ * Latest changed         : 
+ */
+
+import java.awt.Dimension;
+
+import javax.swing.JComponent;
+
+public class Gap extends JComponent {
+	
+	//============================================================== constructor
+    /* Creates filler with minimum size, but expandable infinitely. */
+    public Gap() {
+        Dimension min = new Dimension(0, 0);
+        Dimension max = new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        setMinimumSize(min);
+        setPreferredSize(min);
+        setMaximumSize(max);
+    }
+    
+    //============================================================== constructor
+    /* Creates rigid filler. */
+    public Gap(int size) {
+        Dimension dim = new Dimension(size, size);
+        setMinimumSize(dim);
+        setPreferredSize(dim);
+        setMaximumSize(dim);
+    }
+}
