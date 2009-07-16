e8b5a78b81ef0a297b9d101e84b7daec5b0d7ccd
fredrik_m <afmoller@users.noreply.github.com>
2009-07-16 08:49:52 +0000

    Fixed bug which made this progress bar not to be centered to parent window and removed null layout and introduced GridBagLayout instead.

--- src/moller/javapeg/program/progress/ThumbNailLoading.java
+++ src/moller/javapeg/program/progress/ThumbNailLoading.java
@@ -9,11 +9,13 @@ package moller.javapeg.program.progress;
  *                        : 2009-02-19 by Fredrik Möller
  *                        : 2009-03-06 by Fredrik Möller
  *                        : 2009-03-10 by Fredrik Möller
- *                        : 2009-03-11 by Fredrik Möller 
+ *                        : 2009-03-11 by Fredrik Möller
+ *                        : 2009-07-16 by Fredrik Möller 
  */
 
+import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.Toolkit;
+import java.awt.GridBagLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JDialog;
@@ -21,48 +23,45 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 
+import moller.javapeg.program.GBHelper;
 import moller.javapeg.program.language.Language;
 
 public class ThumbNailLoading extends JDialog{
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 7597859635355677729L;
 	
 	private JProgressBar progress;
 		
-	public ThumbNailLoading(int theMinimumValue, int theMaximumValue){
+	public ThumbNailLoading(int theMinimumValue, int theMaximumValue, Container parent){
 		
 		Language lang = Language.getInstance();
 		
-		// Hämta in skärmstorlek för att kunna positionera progressbarfönstret här nedan
-		Dimension	dScreen		= Toolkit.getDefaultToolkit().getScreenSize();
-		Dimension	dContent 	= new Dimension(630,52);
-
-		// Sätter storlek och position på progressbarfönstret. Fönstret hamnar centrerat på
-		// skärmen.
-		this.setLocation((dScreen.width-dContent.width)/2,(dScreen.height-dContent.height)/2);
-		this.setSize(dContent);
+		int centerXCoordinate = parent.getX() + (parent.getWidth() / 2);
+		int centerYCoordinate = parent.getY() + (parent.getHeight() / 2);
 
 		// Skapar JPanel att lägga alla nedanstående grafikobjekt på.
-		JPanel background = new JPanel(null);
+		JPanel background = new JPanel(new GridBagLayout());
 		background.setBorder(BorderFactory.createRaisedBevelBorder());
 
 		// Skapar JLabel för titel
 		JLabel title = new JLabel(lang.get("progress.ThumbNailLoading.title"));
-		title.setBounds(10,5,110,15);
 
 		// Skapar progressbar
 		progress = new JProgressBar(theMinimumValue,theMaximumValue);
-		progress.setBounds(10,27,610,20);
+		progress.setPreferredSize(new Dimension(610, 20));
+		progress.setMinimumSize(new Dimension(610, 20));
+		progress.setStringPainted(true);
 
+		GBHelper posBackground = new GBHelper();
+		
 		// Lägger alla grafikobjekt på bakgrunden
-		background.add(progress);
-		background.add(title);
-
+		background.add(title, posBackground);
+		background.add(progress, posBackground.nextRow().expandW());
+		
 		// Lägger bakgrunden på ContentPane
 		getContentPane().add(background);
-		
+				
+		this.setBounds(centerXCoordinate - (630 / 2), centerYCoordinate - (52 / 2), 630, 42);
 		this.setUndecorated(true);
 		this.setAlwaysOnTop(true);
 	}
