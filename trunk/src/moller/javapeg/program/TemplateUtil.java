f73caa478072e7aff9fe97e3b166a63e6acfa1a9
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2009-07-22 21:53:37 +0000

    Added a new variable "Original file name" which returns the original file name except the suffix. Code written by Angel Bueno.
    Also updated the subfoldername filter with the above new variable.

--- trunk/src/moller/javapeg/program/TemplateUtil.java
+++ trunk/src/moller/javapeg/program/TemplateUtil.java
@@ -49,22 +49,37 @@ public class TemplateUtil {
 		if(stringToConvert.indexOf("%" + lang.get("variable.apertureValueVariable") + "%") > -1){
 			stringToConvert = stringToConvert.replaceAll("%" + lang.get("variable.apertureValueVariable") + "%", theMetaData.getExifApertureValue());
 		}
-
 		if(stringToConvert.indexOf("%" + lang.get("variable.dateOftodayVariable") + "%") > -1){
 			Calendar cal = new GregorianCalendar();
 
 			String date = "";
 
 			String month = Integer.toString((cal.get(Calendar.MONTH) + 1));
-			if(month.length() == 1)
+			if(month.length() == 1) {
 				month = "0" + month;
+			}
 
 			String dayOfMonth = Integer.toString(cal.get(Calendar.DAY_OF_MONTH));
-			if(dayOfMonth.length() == 1)
+			if(dayOfMonth.length() == 1) {
 				dayOfMonth = "0" + dayOfMonth;
+			}
 
 			date = cal.get(Calendar.YEAR) + "-" + month + "-" + dayOfMonth;
 			stringToConvert = stringToConvert.replaceAll("%" + lang.get("variable.dateOftodayVariable") + "%", date);
+		}
+		/**
+		 * The if statement below was added by Angel Bueno. It gives the 
+		 * possibility to use a variable that inserts the original filename 
+		 * into the resulting filename when the filename template is parsed.
+		 */
+		if(stringToConvert.indexOf("%" + lang.get("variable.sourceNameVariable") + "%") > -1){
+			String fileName = theMetaData.getFileName();
+			if (fileName.toLowerCase().endsWith(".jpeg")) {
+				fileName = fileName.substring(0, fileName.length()-5);				
+			} else {
+				fileName = fileName.substring(0, fileName.length()-4);
+			}
+			stringToConvert = stringToConvert.replaceAll("%" + lang.get("variable.sourceNameVariable") + "%", fileName);
 		}
 
 		return stringToConvert;
@@ -74,24 +89,23 @@ public class TemplateUtil {
 	 * Metod för att kontrollera så att undermappsnamner endast innehåller tillåtna variabler.
 	 **/
 	public static boolean subFolderNameIsValid(String subFolderName) {
-
 		Language lang = Language.getInstance();
 		
-		boolean isValid = true;
-
 		if(subFolderName.indexOf("%" + lang.get("variable.pictureTimeVariable") + "%") > -1)
-			isValid = false;
+			return false;
 		if(subFolderName.indexOf("%" + lang.get("variable.shutterSpeedVariable") + "%") > -1)
-			isValid = false;
+			return false;
 		if(subFolderName.indexOf("%" + lang.get("variable.isoValueVariable") + "%") > -1)
-			isValid = false;
+			return false;
 		if(subFolderName.indexOf("%" + lang.get("variable.pictureWidthVariable") + "%") > -1)
-			isValid = false;
+			return false;
 		if(subFolderName.indexOf("%" + lang.get("variable.pictureHeightVariable") + "%") > -1)
-			isValid = false;
+			return false;
 		if(subFolderName.indexOf("%" + lang.get("variable.apertureValueVariable") + "%") > -1)
-			isValid = false;
+			return false;
+		if(subFolderName.indexOf("%" + lang.get("variable.sourceNameVariable") + "%") > -1)
+			return false;
 
-		return isValid;
+		return true;
 	}
 }
