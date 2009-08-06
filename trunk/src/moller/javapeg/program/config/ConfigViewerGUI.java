b843175b0391a0bd1cf37047497803091de52000
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2009-08-06 21:01:58 +0000

    Added more configuration parameters to the GUI.

--- trunk/src/moller/javapeg/program/config/ConfigViewerGUI.java
+++ trunk/src/moller/javapeg/program/config/ConfigViewerGUI.java
@@ -1,36 +1,42 @@
 package moller.javapeg.program.config;
 /**
  * This class was created : 2009-08-05 by Fredrik Möller
- * Latest changed         : 
+ * Latest changed         : 2009-08-06 by Fredrik Möller
  */
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import java.awt.Point;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.IOException;
 import java.io.InputStream;
 
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
-import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.JTree;
 import javax.swing.UIManager;
 import javax.swing.WindowConstants;
 
 import moller.javapeg.StartJavaPEG;
-import moller.javapeg.program.CustomizedJScrollPane;
-import moller.javapeg.program.helpviewer.HelpViewerGUIUtil;
+import moller.javapeg.program.GBHelper;
+import moller.javapeg.program.Gap;
 import moller.javapeg.program.language.Language;
+import moller.javapeg.program.logger.Level;
 import moller.javapeg.program.logger.Logger;
 import moller.util.gui.Screen;
 import moller.util.io.StreamUtil;
@@ -40,57 +46,84 @@ public class ConfigViewerGUI extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	private JTree tree;
-	
-	private JPanel contentJPanel;
+
 	private JPanel backgroundsPanel;
 	
+	private JPanel loggingConfigurationPanel;
+	private JPanel updatesConfigurationPanel;
+	private JPanel languageConfigurationPanel;
+	
 	private JSplitPane splitPane;
+	
+	private JButton okButton;
+	private JButton applyButton;
+	private JButton cancelButton;
+	
+	
+	
+	/**
+	 * Variables for the logging panel
+	 */
+	private JCheckBox developerMode;
+	private JComboBox logLevels;
+	private JCheckBox rotateLog;
+	
+	private JTextField rotateLogSize;
+	private JTextField logName;
+	private JTextField logEntryTimeStampFormat;
+	
+	/**
+	 * Variables for the updates panel
+	 */
+	private JCheckBox updatesEnabled;
 			
 	private Config   conf;
 	private Logger   logger;
 	private Language lang;
-		
-	private String content = "";
-		
+			
 	public ConfigViewerGUI() {
 		conf   = Config.getInstance();
 		logger = Logger.getInstance();
 		lang   = Language.getInstance();
 				
 		this.initiateWindow();
-		this.addListeners();
+		this.createLoggingConfigurationPanel();
+		this.createUpdateConfigurationPanel();
+		this.addListeners();	
 	}
 	
 	private void initiateWindow() {
 								
-		this.setSize(new Dimension(conf.getIntProperty("helpViewerGUI.window.width"),conf.getIntProperty("helpViewerGUI.window.height")));
+		this.setSize(new Dimension(conf.getIntProperty("configViewerGUI.window.width"),conf.getIntProperty("configViewerGUI.window.height")));
 				
-		Point xyFromConfig = new Point(conf.getIntProperty("helpViewerGUI.window.location.x"),conf.getIntProperty("helpViewerGUI.window.location.y"));
+		Point xyFromConfig = new Point(conf.getIntProperty("configViewerGUI.window.location.x"),conf.getIntProperty("configViewerGUI.window.location.y"));
 				
 		if(Screen.isOnScreen(xyFromConfig)) {
 			this.setLocation(xyFromConfig);
 		} else {
 			this.setLocation(0,0);
+//			TODO: Change errror message
 			JOptionPane.showMessageDialog(null, lang.get("helpViewerGUI.window.locationError"), lang.get("errormessage.maingui.errorMessageLabel"), JOptionPane.ERROR_MESSAGE);
-			logger.logERROR("Could not set location of Help Viewer GUI to: x = " + xyFromConfig.x + " and y = " + xyFromConfig.y + " since that is outside of available screen size.");
+			logger.logERROR("Could not set location of Config Viewer GUI to: x = " + xyFromConfig.x + " and y = " + xyFromConfig.y + " since that is outside of available screen size.");
 		}
 		this.setLayout(new BorderLayout());
 		
 		try{
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e){
-			logger.logERROR("Could not set desired Look And Feel for Help Viewer GUI");
+			logger.logERROR("Could not set desired Look And Feel for Config Viewer GUI");
 			logger.logERROR("Below is the generated StackTrace");
 			
 			for(StackTraceElement element : e.getStackTrace()) {
 				logger.logERROR(element.toString());	
 			}
 		}
-		this.getContentPane().add(this.initiateSplitPane(), BorderLayout.CENTER);	
+		this.getContentPane().add(this.initiateSplitPane(), BorderLayout.CENTER);
+		this.getContentPane().add(this.createButtonPanel(), BorderLayout.SOUTH);
 		
 		InputStream imageStream = null;
 		try {
-			imageStream = StartJavaPEG.class.getResourceAsStream("resources/images/Help16.gif");
+			imageStream = StartJavaPEG.class.getResourceAsStream("resources/images/configuration.gif");
 			this.setIconImage(ImageIO.read(imageStream));
 		} catch (Exception e) {
 			Logger.getInstance().logERROR("Could not open the image Help16.gif");
@@ -98,13 +131,13 @@ public class ConfigViewerGUI extends JFrame {
 			StreamUtil.closeStream(imageStream);
 		}
 		
-		this.setTitle(lang.get("helpViewerGUI.window.title"));
+//		TODO: Fix hard coded string
+		this.setTitle("Configuration");
 	}
 	
 	private JSplitPane initiateSplitPane() {	
 		backgroundsPanel = new JPanel(new BorderLayout());
-		backgroundsPanel.add(this.initiateContentPanel(), BorderLayout.CENTER);
-		
+				
 		splitPane = new JSplitPane();
 		splitPane.setOneTouchExpandable(true);
 		splitPane.setDividerLocation(200);
@@ -116,8 +149,215 @@ public class ConfigViewerGUI extends JFrame {
 	
 	private void addListeners(){
 		this.addWindowListener(new WindowEventHandler());
-		backgroundsPanel.addComponentListener(new WindowComponentListener());
+		okButton.addActionListener(new OkButtonListener());
+		applyButton.addActionListener(new ApplyButtonListener());
+		cancelButton.addActionListener(new CancelButtonListener());
 	}
+	
+	private JPanel createButtonPanel() {
+		JPanel buttonPanel = new JPanel();
+
+//		TODO: Remove hard coded strings
+		okButton = new JButton("Ok");
+		applyButton = new JButton("Apply");
+		cancelButton = new JButton("Cancel");
+		
+		buttonPanel.add(okButton);
+		buttonPanel.add(applyButton);
+		buttonPanel.add(cancelButton);
+		
+		return buttonPanel;
+	}
+	
+	private void createLoggingConfigurationPanel() {
+		loggingConfigurationPanel = new JPanel(new GridBagLayout());
+		
+		GBHelper posLoggingPanel = new GBHelper();
+						
+		String logLevel = conf.getStringProperty("logger.log.level");
+		int seletedIndex = 0; 
+		
+		for (Level level : Level.values()) {
+			if(level.toString().equals(logLevel)) {
+				seletedIndex = level.ordinal();
+				break;
+			}	
+		}
+//		TODO: Remove hard coded string
+		JLabel logLevelsLabel = new JLabel("Log Levels");
+		logLevels = new JComboBox(Level.values());				
+		logLevels.setSelectedIndex(seletedIndex);
+		
+//		TODO: Remove hard coded string
+		JLabel developerModeLabel = new JLabel("Unbuffered Logging");
+		developerMode = new JCheckBox();
+		developerMode.setSelected(conf.getBooleanProperty("logger.developerMode"));
+				
+//		TODO: Remove hard coded string
+		JLabel rotateLogLabel = new JLabel("Rotate Log");
+		rotateLog = new JCheckBox();
+		rotateLog.setSelected(conf.getBooleanProperty("logger.log.rotate"));
+		
+//		TODO: Remove hard coded string
+		JLabel rotateLogSizeLabel = new JLabel("Rotate Log Size");
+		rotateLogSize = new JTextField();
+		rotateLogSize.setText(conf.getStringProperty("logger.log.rotate.size"));
+
+//		TODO: Remove hard coded string
+		JLabel logNameLabel = new JLabel("Log Name");
+		logName = new JTextField();
+		logName.setText(conf.getStringProperty("logger.log.name"));
+		
+//		TODO: Remove hard coded string
+		JLabel logEntryTimeStampFormatLabel = new JLabel("Log Entry Timestamp Format");
+		logEntryTimeStampFormat = new JTextField();
+		logEntryTimeStampFormat.setText(conf.getStringProperty("logger.log.entry.timestamp.format"));
+				
+		loggingConfigurationPanel.add(developerModeLabel, posLoggingPanel);
+		loggingConfigurationPanel.add(new Gap(10), posLoggingPanel.nextCol());
+		loggingConfigurationPanel.add(developerMode, posLoggingPanel.nextCol());
+		loggingConfigurationPanel.add(new Gap(10), posLoggingPanel.nextRow());
+		loggingConfigurationPanel.add(rotateLogLabel, posLoggingPanel.nextRow());
+		loggingConfigurationPanel.add(new Gap(10), posLoggingPanel.nextCol());
+		loggingConfigurationPanel.add(rotateLog, posLoggingPanel.nextCol());
+		loggingConfigurationPanel.add(new Gap(10), posLoggingPanel.nextRow());
+		loggingConfigurationPanel.add(rotateLogSizeLabel, posLoggingPanel.nextRow());
+		loggingConfigurationPanel.add(new Gap(10), posLoggingPanel.nextCol());
+		loggingConfigurationPanel.add(rotateLogSize, posLoggingPanel.nextCol());
+		loggingConfigurationPanel.add(new Gap(10), posLoggingPanel.nextRow());
+		loggingConfigurationPanel.add(logLevelsLabel, posLoggingPanel.nextRow());
+		loggingConfigurationPanel.add(new Gap(10), posLoggingPanel.nextCol());
+		loggingConfigurationPanel.add(logLevels, posLoggingPanel.nextCol());
+		loggingConfigurationPanel.add(new Gap(10), posLoggingPanel.nextRow());
+		loggingConfigurationPanel.add(logNameLabel, posLoggingPanel.nextRow());
+		loggingConfigurationPanel.add(new Gap(10), posLoggingPanel.nextCol());
+		loggingConfigurationPanel.add(logName, posLoggingPanel.nextCol());
+		loggingConfigurationPanel.add(new Gap(10), posLoggingPanel.nextRow());
+		loggingConfigurationPanel.add(logEntryTimeStampFormatLabel, posLoggingPanel.nextRow());
+		loggingConfigurationPanel.add(new Gap(10), posLoggingPanel.nextCol());
+		loggingConfigurationPanel.add(logEntryTimeStampFormat, posLoggingPanel.nextCol());	
+	}
+	
+	private void createUpdateConfigurationPanel() {
+		updatesConfigurationPanel = new JPanel(new GridBagLayout());
+		
+		GBHelper posUpdatesPanel = new GBHelper();
+		
+		JLabel updatesEnabledLabel = new JLabel("Enable Application Update Check");
+		updatesEnabled = new JCheckBox();
+		updatesEnabled.setSelected(conf.getBooleanProperty("updatechecker.enabled"));
+		
+		
+		updatesConfigurationPanel.add(updatesEnabledLabel, posUpdatesPanel);
+		updatesConfigurationPanel.add(new Gap(10), posUpdatesPanel.nextCol());
+		updatesConfigurationPanel.add(updatesEnabled, posUpdatesPanel.nextCol());
+		
+		
+		
+	}
+	
+	private void createLanguageConfigurationPanel() {
+		
+		languageConfigurationPanel = new JPanel(new GridBagLayout());
+		
+		GBHelper posLanguagePanel = new GBHelper();
+		
+		JLabel languageSelectionMethod = new JLabel("SPRÅKVALSMETOD");
+		languageSelectionMethod.setForeground(Color.GRAY);
+		
+		JLabel availableLanguages = new JLabel("TILLGÄNGLIGA SPRÅK");
+		availableLanguages.setForeground(Color.GRAY);
+		
+		languageConfigurationPanel.add(languageSelectionMethod, posLanguagePanel);
+		languageConfigurationPanel.add(availableLanguages, posLanguagePanel.nextCol());
+		
+	}
+		
+	private void updateWindowLocationAndSize() {
+		conf.setIntProperty("configViewerGUI.window.location.x", this.getLocation().x);
+		conf.setIntProperty("configViewerGUI.window.location.y", this.getLocation().y);
+		conf.setIntProperty("configViewerGUI.window.width", this.getSize().width);
+		conf.setIntProperty("configViewerGUI.window.height", this.getSize().height);	
+	}
+		
+	private JScrollPane initiateJTree() {		
+		tree = new JTree(ConfigViewerGUIUtil.createNodes());
+		tree.setShowsRootHandles (true);
+		tree.addMouseListener(new Mouselistener());
+		
+		return new JScrollPane(tree);
+	}
+	
+	private void updateConfiguration() {
+		/**
+		 * Update Logging Configuration
+		 */
+		conf.setStringProperty("logger.log.level", logLevels.getSelectedItem().toString());
+		conf.setBooleanProperty("logger.developerMode", developerMode.isSelected());
+		conf.setBooleanProperty("logger.log.rotate", rotateLog.isSelected());
+		conf.setStringProperty("logger.log.rotate.size", rotateLogSize.getText());
+		conf.setStringProperty("logger.log.name", logName.getText());
+		conf.setStringProperty("logger.log.entry.timestamp.format", logEntryTimeStampFormat.getText());
+		
+		/**
+		 * Update Updates Configuration
+		 */
+		conf.setBooleanProperty("updatechecker.enabled", updatesEnabled.isSelected());
+	}
+	
+	private void closeWindow() {
+		updateWindowLocationAndSize();
+		this.setVisible(false);
+		this.dispose();
+	}
+				
+	/**
+	 * Mouse listener
+	 */
+	private class Mouselistener extends MouseAdapter{
+		public void mousePressed(MouseEvent e){
+			int selRow = tree.getRowForLocation(e.getX(), e.getY());
+			if(selRow > -1) {
+							
+				backgroundsPanel.removeAll();
+				backgroundsPanel.updateUI();
+				
+				if (selRow == 1) {
+					backgroundsPanel.add(loggingConfigurationPanel);
+				} else if (selRow == 2) {
+					backgroundsPanel.add(updatesConfigurationPanel);
+				}
+			}
+		}
+	}
+	
+	private class OkButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			updateConfiguration();
+			closeWindow();
+		}
+		
+	}
+
+	private class ApplyButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			updateConfiguration();
+			
+		}
+		
+	}
+
+	private class CancelButtonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			closeWindow();
+		}
+	}
+
 	
 	private class WindowEventHandler extends WindowAdapter {
 		public void windowClosing(WindowEvent e) {
@@ -125,76 +365,4 @@ public class ConfigViewerGUI extends JFrame {
 			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		}
 	}
-	
-	private void updateWindowLocationAndSize() {
-		conf.setIntProperty("helpViewerGUI.window.location.x", this.getLocation().x);
-		conf.setIntProperty("helpViewerGUI.window.location.y", this.getLocation().y);
-		conf.setIntProperty("helpViewerGUI.window.width", this.getSize().width);
-		conf.setIntProperty("helpViewerGUI.window.height", this.getSize().height);	
-	}
-		
-	private JScrollPane initiateJTree() {		
-		tree = new JTree(HelpViewerGUIUtil.createNodes());
-		tree.setShowsRootHandles (true);
-		tree.addMouseListener(new Mouselistener());
-		
-		return new JScrollPane(tree);
-	}
-	
-	private CustomizedJScrollPane initiateContentPanel() {
-		contentJPanel = new JPanel(new BorderLayout());
-		return new CustomizedJScrollPane(contentJPanel);
-	}
-		
-	private JTextArea getContent(int selectedRow) {
-		if (content.equals("") && selectedRow > -1) {
-			String confLang = conf.getStringProperty("gUILanguageISO6391");
-							
-			try {
-				content = StreamUtil.getString(StartJavaPEG.class.getResourceAsStream("resources/help/" + confLang + HelpViewerGUIUtil.getFile(selectedRow)));
-			} catch (IOException e) {
-				
-				JOptionPane.showMessageDialog(null, lang.get("helpViewerGUI.errorMessage"), lang.get("errormessage.maingui.errorMessageLabel"), JOptionPane.ERROR_MESSAGE);
-				logger.logERROR("Could not load embedded help file:");
-				logger.logERROR(e);
-			}
-			content = content.replaceAll("\r", "");
-			content = content.replaceAll("\n", "");
-			content = content.replaceAll("<LF>", System.getProperty("line.separator"));
-		}
-		
-		JTextArea textarea = new JTextArea();
-		textarea.setLineWrap(true);
-		textarea.setWrapStyleWord(true);
-		textarea.setEditable(false);
-		textarea.setText(content);
-		textarea.setCaretPosition(0);
-		
-		return textarea;
-	}
-			
-	/**
-	 * Mouse listener
-	 */
-	private class Mouselistener extends MouseAdapter{
-		public void mousePressed(MouseEvent e){
-			int selRow = tree.getRowForLocation(e.getX(), e.getY());
-			if(selRow > 0) {
-				content = "";
-				contentJPanel.removeAll();
-				contentJPanel.updateUI();
-				contentJPanel.add(getContent(selRow), BorderLayout.CENTER);
-			}
-		}
-	}
-		
-	private class WindowComponentListener extends ComponentAdapter {
-		@Override
-		public void componentResized(ComponentEvent e) {
-			contentJPanel.removeAll();
-			contentJPanel.updateUI();
-			contentJPanel.add(getContent(-1), BorderLayout.CENTER);
-		}		
-	}
-
 }
