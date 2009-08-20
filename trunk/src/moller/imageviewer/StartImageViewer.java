7eabab383485953f44de3b7f243d024201ad2efd
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2009-08-20 20:12:26 +0000

    Added support for having the same language loaded as in the JavaPEG application.

--- trunk/src/moller/imageviewer/StartImageViewer.java
+++ trunk/src/moller/imageviewer/StartImageViewer.java
@@ -2,6 +2,7 @@ package moller.imageviewer;
 /**
  * This class was created : 2009-08-15 by Fredrik Möller
  * Latest changed         : 2009-08-16 by Fredrik Möller
+ *                        : 2009-08-20 by Fredrik Möller
  */
 
 import java.io.File;
@@ -9,20 +10,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 import moller.imageviewer.program.gui.ImageViewer;
+import moller.imageviewer.program.language.ImageViewerLanguage;
 
 public class StartImageViewer {
 
 	/**
-	 * @param args
+	 * @param args contains the arguments provided by the calling object.
+	 *        
+	 *        Content of the argument array "args":
+     *
+	 *	      index: 0     = Language code in ISO639 format
+	 *               1...n = Absolute paths to images
 	 */
 	public static void main(String[] args) {
-				
-		List<File> images = new ArrayList<File>(args.length);
 		
-		for (int i = 0; i < args.length; i++) {
+		List<File> images = new ArrayList<File>(args.length);
+				
+		ImageViewerLanguage imageViewerLanguage = ImageViewerLanguage.getInstance();
+		imageViewerLanguage.loadLanguageFile(args[0]);
+		
+		for (int i = 1; i < args.length; i++) {
 			images.add(new File(args[i]));
 		}
-				
+			
 		ImageViewer imageViewer  = new ImageViewer(images);
 		imageViewer.setVisible(true);
 	}
