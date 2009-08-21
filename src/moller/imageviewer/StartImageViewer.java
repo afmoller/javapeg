f5ee3c6f1514ac4d9c363d4265078e11b770e437
fredrik_m <afmoller@users.noreply.github.com>
2009-08-21 21:46:16 +0000

    Added support for initializing a Logger for the ImageViewer.

--- src/moller/imageviewer/StartImageViewer.java
+++ src/moller/imageviewer/StartImageViewer.java
@@ -3,6 +3,7 @@ package moller.imageviewer;
  * This class was created : 2009-08-15 by Fredrik Möller
  * Latest changed         : 2009-08-16 by Fredrik Möller
  *                        : 2009-08-20 by Fredrik Möller
+ *                        : 2009-08-21 by Fredrik Möller
  */
 
 import java.io.File;
@@ -11,6 +12,7 @@ import java.util.List;
 
 import moller.imageviewer.program.gui.ImageViewer;
 import moller.imageviewer.program.language.ImageViewerLanguage;
+import moller.util.logger.Logger;
 
 public class StartImageViewer {
 
@@ -19,17 +21,49 @@ public class StartImageViewer {
 	 *        
 	 *        Content of the argument array "args":
      *
-	 *	      index: 0     = Language code in ISO639 format
-	 *               1...n = Absolute paths to images
+	 *	      index: 0     = Language code in ISO639 format.
+	 *
+	 *               1     = A boolean value indicating whether the ImageViewer
+	 *                       log shall be automatically rotated or not.
+	 *                       
+	 *               2     = A boolean value indicating whether the ImageViewer
+	 *                       log shall be written to unbuffered or not.
+	 *                       
+	 *               3     = The name of the file containing the ImageViewer 
+	 *                       log.
+	 *               
+	 *               4     = A SimpleDateFormat String defining how the 
+	 *                       timestamp for each log entry in the ImageViewer 
+	 *                       log shall look like.
+	 *                      
+	 *               5     = Which log level that shall be used by the 
+	 *                       ImageViewer log.
+	 *               
+	 *               6     = The size in bytes when the automatically log
+	 *                       rotation takes place.
+	 *                       
+	 *               7...n = Absolute paths to images
 	 */
 	public static void main(String[] args) {
 		
+		boolean rotateLog      = args[1].equals("true") ? true : false;
+    	boolean developerMode  = args[2].equals("true") ? true : false;
+    	
+    	String logName         = args[3];
+        String timeStampFormat = args[4];
+        String logLevel        = args[5];
+		
+        int	rotateSize         = Integer.parseInt(args[6]);
+        		        
+		Logger logger = Logger.getInstance();
+		logger.initiateLogger(rotateLog, developerMode, rotateSize, logName, timeStampFormat, logLevel);
+				
 		List<File> images = new ArrayList<File>(args.length);
 				
 		ImageViewerLanguage imageViewerLanguage = ImageViewerLanguage.getInstance();
 		imageViewerLanguage.loadLanguageFile(args[0]);
-		
-		for (int i = 1; i < args.length; i++) {
+				
+		for (int i = 7; i < args.length; i++) {
 			images.add(new File(args[i]));
 		}
 			
