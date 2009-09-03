78589b447adfdda40d2154cba9c60844657037a9
fredrik_m <afmoller@users.noreply.github.com>
2009-09-03 19:50:01 +0000

    Added an registration of an UncaughtExceptionHandler

--- src/moller/javapeg/StartJavaPEG.java
+++ src/moller/javapeg/StartJavaPEG.java
@@ -1,11 +1,18 @@
 package moller.javapeg;
+/**
+ * This class was created : 2009-02-25 by Fredrik Möller
+ * Latest changed         : 2009-09-03 by Fredrik Möller
+ */
 
 import moller.javapeg.program.MainGUI;
+import moller.javapeg.program.ApplicationUncaughtExceptionHandler;
 
 public class StartJavaPEG {
 	
 	public static void main (String [] args){
 	
+		ApplicationUncaughtExceptionHandler.registerExceptionHandler();
+		
 		MainGUI mainGUI = new MainGUI();
 		mainGUI.setVisible(true);
 	}
