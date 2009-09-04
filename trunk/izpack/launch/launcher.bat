88f2cffd01324465de3b1bc3c6131f079e17a98c
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2009-09-04 21:38:18 +0000

    Added comments in the file.

--- trunk/izpack/launch/launcher.bat
+++ trunk/izpack/launch/launcher.bat
@@ -1,1 +1,5 @@
-java -jar -Xms64m -Xmx256m javapeg.jar
+REM This file will start JavaPEG with custom memory settings for the JVM. With 
+REM the below settings the heap size (Available memory for the application) will
+REM range from 64 megabyte up to 256 megabyte.
+
+java -jar -Xms64m -Xmx256m JavaPEG.jar
