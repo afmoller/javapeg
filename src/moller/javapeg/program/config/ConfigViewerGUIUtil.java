89a990012352bea4d71074ab5806c9a2b4ae613d
fredrik_m <afmoller@users.noreply.github.com>
2009-09-06 20:03:58 +0000

    Made the strings in the tree structure localizable.

--- src/moller/javapeg/program/config/ConfigViewerGUIUtil.java
+++ src/moller/javapeg/program/config/ConfigViewerGUIUtil.java
@@ -1,32 +1,31 @@
 package moller.javapeg.program.config;
 /**
  * This class was created : 2009-08-06 by Fredrik Möller
- * Latest changed         : 
+ * Latest changed         : 2009-09-06 by Fredrik Möller
  */
 
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeNode;
 
+import moller.javapeg.program.language.Language;
+
 public class ConfigViewerGUIUtil {
 	
 public static TreeNode createNodes() {
 		
-//		Language lang = Language.getInstance();
+		Language lang = Language.getInstance();
 
-//	TODO: Remove hard coded strings
-		DefaultMutableTreeNode root    = new DefaultMutableTreeNode("Configuration");
-		DefaultMutableTreeNode logging = new DefaultMutableTreeNode("Logging");
-		DefaultMutableTreeNode updates = new DefaultMutableTreeNode("Updates");
-		DefaultMutableTreeNode rename  = new DefaultMutableTreeNode("Rename");
-		DefaultMutableTreeNode language  = new DefaultMutableTreeNode("Language");
+		DefaultMutableTreeNode root     = new DefaultMutableTreeNode(lang.get("configviewer.tree.root"));
+		DefaultMutableTreeNode logging  = new DefaultMutableTreeNode(lang.get("configviewer.tree.node.logging"));
+		DefaultMutableTreeNode updates  = new DefaultMutableTreeNode(lang.get("configviewer.tree.node.updates"));
+		DefaultMutableTreeNode rename   = new DefaultMutableTreeNode(lang.get("configviewer.tree.node.rename"));
+		DefaultMutableTreeNode language = new DefaultMutableTreeNode(lang.get("configviewer.tree.node.language"));
 				
 		root.add(logging);
 		root.add(updates);
 		root.add(rename);
 		root.add(language);
-		
-		
-				
+					
 		return root;	
 	}
 	
@@ -57,5 +56,4 @@ public static TreeNode createNodes() {
 		}
 		return fileToLoad;
 	}
-
 }
