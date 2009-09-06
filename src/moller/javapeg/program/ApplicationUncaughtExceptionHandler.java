1ee0e0b2f85242358e86204271cf11cb880612ce
fredrik_m <afmoller@users.noreply.github.com>
2009-09-06 20:05:51 +0000

    Added logging, System.exit(1) of the application and extended error message.

--- src/moller/javapeg/program/ApplicationUncaughtExceptionHandler.java
+++ src/moller/javapeg/program/ApplicationUncaughtExceptionHandler.java
@@ -2,10 +2,13 @@ package moller.javapeg.program;
 /**
  * This class was created : 2009-09-02 by Fredrik Möller
  * Latest changed         : 2009-09-03 by Fredrik Möller
+ *                        : 2009-09-06 by Fredrik Möller  
  */
 
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+
+import moller.javapeg.program.logger.Logger;
 
 public class ApplicationUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
 
@@ -25,20 +28,29 @@ public class ApplicationUncaughtExceptionHandler implements Thread.UncaughtExcep
 				});
 			}
 		} catch (Throwable t) {
+			System.exit(1);
 			// don't let the exception get thrown out, will cause infinite looping!
-		}
+		} 
 	}
 
-    private void showException(Throwable throwable) {
+    private void showException(Throwable t) {
+    	logException(t);
     	
-        String msg = String.format("Unexpected problem on thread %s", throwable.getMessage());
-
-        logException(throwable);
-        JOptionPane.showMessageDialog(null, msg);
+    	String errorMessage = "";
+    	
+//    	TODO: Remove hard coded strings
+    	if("Java heap space".equals(t.getMessage())) {
+    		errorMessage = "JavaPEG has run out of memory.\n\nSee logfile for more details and consult the README file\nin installationdirectory to find a solution\n\nJavaPEG will be shut down";
+    	} else {
+    		errorMessage = "An unexpected error has occured: " + t.getMessage() + ".\n\nSee log file for details and consult the README file\nin installationdirectory to find a solution\n\nJavaPEG will be shut down";
+    	}
+        JOptionPane.showMessageDialog(null, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+        System.exit(1);
     }
 
     private void logException(Throwable throwable) {
-//        TODO: add logging
+    	Logger.getInstance().logFATAL(throwable);
+    	Logger.getInstance().flush();
     }
     
     public static void registerExceptionHandler() {
