703429340ab09d5c3778992f9955865810e31e47
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2009-09-20 18:37:19 +0000

    Removed obsolete files.

--- trunk/izpack/launch/launcher.bat
+++
@@ -1,5 +0,0 @@
-REM This file will start JavaPEG with custom memory settings for the JVM. With 
-REM the below settings the heap size (Available memory for the application) will
-REM range from 64 megabyte up to 384 megabyte.
-
-javaw -jar -Xms64m -Xmx384m JavaPEG.jar
