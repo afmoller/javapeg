f76637a6e23abf13d1554571235a9fe0f0b5340d
fredrik_m <afmoller@users.noreply.github.com>
2009-09-20 13:36:39 +0000

    Changed -Xmx from 256 to 384.

--- izpack/launch/launcher.bat
+++ izpack/launch/launcher.bat
@@ -1,5 +1,5 @@
 REM This file will start JavaPEG with custom memory settings for the JVM. With 
 REM the below settings the heap size (Available memory for the application) will
-REM range from 64 megabyte up to 256 megabyte.
+REM range from 64 megabyte up to 384 megabyte.
 
-java -jar -Xms64m -Xmx256m JavaPEG.jar
+javaw -jar -Xms64m -Xmx384m JavaPEG.jar
--- izpack/launch/launcher.sh
+++ izpack/launch/launcher.sh
@@ -2,6 +2,6 @@
 
 # This file will start JavaPEG with custom memory settings for the JVM. With 
 # the below settings the heap size (Available memory for the application) will
-# range from 64 megabyte up to 256 megabyte.
+# range from 64 megabyte up to 384 megabyte.
 
-java -jar -Xms64m -Xmx256m JavaPEG.jar
+javaw -jar -Xms64m -Xmx384m JavaPEG.jar
