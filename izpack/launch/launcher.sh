d495686a17345dec0ebceb678fd5e322cb999bc4
fredrik_m <afmoller@users.noreply.github.com>
2009-09-20 18:37:19 +0000

    Removed obsolete files.

--- izpack/launch/launcher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This file will start JavaPEG with custom memory settings for the JVM. With 
-# the below settings the heap size (Available memory for the application) will
-# range from 64 megabyte up to 384 megabyte.
-
-javaw -jar -Xms64m -Xmx384m JavaPEG.jar
