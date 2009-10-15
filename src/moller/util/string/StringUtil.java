92dd49d2d1173cc4f3f8afa58d50e86c01bb841f
fredrik_m <afmoller@users.noreply.github.com>
2009-10-15 20:12:15 +0000

    Added utility string method that removes all \n, \r and converts <LF> to system line separator in a string, and a unit test of this method.

--- src/moller/util/string/StringUtil.java
+++ src/moller/util/string/StringUtil.java
@@ -1,7 +1,7 @@
 package moller.util.string;
 /**
  * This class was created : 2009-04-06 by Fredrik Möller
- * Latest changed         :
+ * Latest changed         : 2009-10-15 by Fredrik Möller
  */
 
 import java.text.DecimalFormat;
@@ -25,5 +25,19 @@ public class StringUtil {
 		if (bytes < Math.pow(1000, 3))
 			return df.format(bytes / Math.pow(1024, 2)) + " MiByte";
 		return df.format(bytes / Math.pow(1024, 3)) + " GiByte";
+	}
+	
+	/**
+	 * This method takes a String as input an removes all newline characters
+	 * in it and changes any occurrences of the string "<LF>" with the line 
+	 * separator for the current system.
+	 * 
+	 * @param s is the string to format.
+	 * @return a formatted string.
+	 */
+	public static String formatString(String s) {
+		s = s.replaceAll("\r", "");
+		s = s.replaceAll("\n", "");
+		return s.replaceAll("<LF>", System.getProperty("line.separator"));
 	}
 }
