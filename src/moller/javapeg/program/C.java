56dea28dff221afb71f3f76eb75405d578e3a394
fredrik_m <afmoller@users.noreply.github.com>
2009-11-14 16:48:45 +0000

    Added a class which holds application global constants.

---
+++ src/moller/javapeg/program/C.java
@@ -0,0 +1,25 @@
+package moller.javapeg.program;
+/**
+ * This class was created : 2009-11-13 by Fredrik Möller
+ * Latest changed         : 2009-11-14 by Fredrik Möller
+ */
+
+import java.io.File;
+
+public class C {
+	
+	/**
+	 * The system dependent file separator char
+	 */
+	public final static String FS              = File.separator;
+	
+	public final static String USER_HOME       = System.getProperty("user.home");
+	public final static String JAVAPEG_VERSION = "2.4";
+	
+	/**
+	 * This variable  contains a time stamp which is set to when the latest 
+	 * release is done. The value is the amount of milliseconds since 
+	 * 1970-01-01 with 
+	 */
+	public final static long VERSION_TIMESTAMP = 1249304521;
+}
