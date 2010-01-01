b084f072f972debca9c06b0a120627a6cc58e1a8
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2010-01-01 22:10:12 +0000

    An Enum describing states of a path.

---
+++ trunk/src/moller/util/io/Status.java
@@ -0,0 +1,11 @@
+package moller.util.io;
+/**
+ * This class was created : 2009-12-20 by Fredrik Möller
+ * Latest changed         : 
+ */
+
+public enum Status {
+	EXISTS, 
+	NOT_AVAILABLE,
+	DOES_NOT_EXIST
+}
