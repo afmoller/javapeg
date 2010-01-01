6012dac315e123bc25ccb64e55a8d485572f46ab
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2010-01-01 22:03:31 +0000

    Added a string reverse method.

--- trunk/src/moller/util/test/unittest/string/TestStringUtil.java
+++ trunk/src/moller/util/test/unittest/string/TestStringUtil.java
@@ -1,4 +1,8 @@
 package moller.util.test.unittest.string;
+/**
+ * This class was created : 2009-12-28 by Fredrik Möller
+ * Latest changed         : 
+ */
 
 import junit.framework.Assert;
 import moller.util.string.StringUtil;
@@ -15,5 +19,10 @@ public class TestStringUtil {
 	@Test
 	public void testFormatString() {
 		Assert.assertEquals("TestTestTest" + System.getProperty("line.separator"), StringUtil.formatString("Test\nTest\rTest<LF>"));		
+	}
+	
+	@Test
+	public void testReverse() {
+		Assert.assertEquals("tset", StringUtil.reverse("test"));		
 	}
 }
