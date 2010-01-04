43228e79fbcee1bb918c2242382b49ae1c81b2ea
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2010-01-04 21:18:47 +0000

    Made some refactoring, including improved JPEG validation. Not only search for ffd8 at the start of the file but also for ffd9 at the end of the file. A new thumbnail creation method has been added.

--- trunk/src/moller/util/io/JPEGUtil.java
+++ trunk/src/moller/util/io/JPEGUtil.java
@@ -2,12 +2,20 @@ package moller.util.io;
 /**
  * This class was created : 2009-03-27 by Fredrik Möller
  * Latest changed         : 2009-06-17 by Fredrik Möller
+ *                        : 2010-01-02 by Fredrik Möller
+ *                        : 2010-01-03 by Fredrik Möller
+ *                        : 2010-01-04 by Fredrik Möller
  */
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class JPEGUtil {
 
@@ -24,14 +32,26 @@ public class JPEGUtil {
 	 */
 	public static boolean isJPEG(File file) throws FileNotFoundException, IOException{
 		if (file.isFile() && file.canRead()) {
-			byte[]mB = new byte[2];
-
-			FileInputStream fis = new FileInputStream(file);
-			fis.read(mB);
-
-			String fName = file.getName();
-			String ext = fName.substring(fName.indexOf(".") + 1);
-
+			
+			byte [] ffd8 = new byte [2];
+			byte [] ffd9 = new byte [2];
+			
+			FileInputStream fis = null;
+			
+			try {
+				fis = new FileInputStream(file);
+				
+				fis.read(ffd8);
+				fis.skip(file.length() - 2 - 2);
+				fis.read(ffd9);
+			} finally {
+				 if(fis != null) {
+					 fis.close();
+				 }
+			}
+			
+			String fileName = file.getName();
+			
 			/**
 			 * Här kontrolleras så att de två första byten i filen har värdet 255 (FF)
 			 * och 216 (D8) vilket är det som identifierar en JPEG/JFIF-fil. I if-satsen
@@ -39,12 +59,56 @@ public class JPEGUtil {
 			 * filnamnet slutar på JPG eller JPEG så att inte tumnaglar eller liknande med
 			 * annan filändelse slinker igenom. 
 			 */
-			return ((mB[0] & 0xFF) == 255 && 
-					(mB[1] & 0xFF) == 216) && 
-					((ext.equalsIgnoreCase("jpg") || 
-					  ext.equalsIgnoreCase("jpeg")));
+			return (startsWithFFD8(ffd8) && endsWithFFD9(ffd9) && hasJFIFExtension(fileName));
 		} else {
 			return false;
 		}
-	}	
+	}
+	
+	/**
+	 * @param jpegFile
+	 * @param width
+	 * @param height
+	 * @return
+	 * @throws IOException
+	 */
+	public static ByteArrayOutputStream createThumbNail(File jpegFile, int width, int height) throws IOException {
+		
+		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		img.createGraphics().drawImage(ImageIO.read(jpegFile).getScaledInstance(width, height, Image.SCALE_SMOOTH),0,0,null);
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ImageIO.write(img, "jpg", baos);
+		
+		return baos;
+	}
+	
+	/**
+	 * @param data
+	 * @return
+	 */
+	public static boolean isJPEG(byte [] data) {
+		return startsWithFFD8(data) && endsWithFFD9(data);
+	}
+	
+	/**
+	 * @param data
+	 * @return
+	 */
+	private static boolean startsWithFFD8(byte [] data) {
+		return (data[0] & 0xFF) == 255 && (data[1] & 0xFF) == 216;
+	}
+	
+	private static boolean endsWithFFD9(byte [] data) {
+		return (data[data.length - 2] & 0xFF) == 255 && (data[data.length - 1] & 0xFF) == 217;
+	}
+	
+	/**
+	 * @param fileName
+	 * @return
+	 */
+	private static boolean hasJFIFExtension(String fileName) {
+		String ext = fileName.substring(fileName.indexOf(".") + 1).toLowerCase();
+		return ext.equals("jpg") || ext.equals("jpeg");
+	}
 }
