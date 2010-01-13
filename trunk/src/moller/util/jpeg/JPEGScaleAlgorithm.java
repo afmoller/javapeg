2489c824f2476f644d2299c7254e6d5b249aa4f4
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2010-01-13 21:46:52 +0000

    Added a enumeration of available scale methods.

---
+++ trunk/src/moller/util/jpeg/JPEGScaleAlgorithm.java
@@ -0,0 +1,11 @@
+package moller.util.jpeg;
+
+/**
+ * This class was created : 2010-01-13 by Fredrik Möller
+ * Latest changed         : 
+ */
+
+public enum JPEGScaleAlgorithm {
+	SMOOTH,
+	FAST;
+}
