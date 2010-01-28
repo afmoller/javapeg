34c0c472130168ceed7783884a51bd0bb2316844
fredrik_m <afmoller@users.noreply.github.com>
2010-01-28 21:21:03 +0000

    Makes use if the ImageRepositoryItem instead of parsing Strings. An effect of the use of storing ImageRepositoryItems instead of Strings in the data model which this class renders.

--- src/moller/javapeg/program/gui/CustomCellRenderer.java
+++ src/moller/javapeg/program/gui/CustomCellRenderer.java
@@ -2,6 +2,7 @@ package moller.javapeg.program.gui;
 /**
  * This class was created : 2009-12-25 by Fredrik Möller
  * Latest changed         : 2009-12-28 by Fredrik Möller
+ *                        : 2010-01-28 by Fredrik Möller
  */
 
 import java.awt.Color;
@@ -12,15 +13,11 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 
-import moller.javapeg.program.C;
+import moller.javapeg.program.categories.ImageRepositoryItem;
 import moller.util.io.Status;
-import moller.util.string.StringUtil;
 
 public class CustomCellRenderer	extends	JLabel implements ListCellRenderer {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public CustomCellRenderer() {
@@ -30,26 +27,24 @@ public class CustomCellRenderer	extends	JLabel implements ListCellRenderer {
 	public Component getListCellRendererComponent(JList list, Object value, int index, 
 			                                      boolean isSelected, boolean cellHasFocus) {
 		
-		String stringValue = value.toString();
-		
-		stringValue =  StringUtil.reverse(stringValue);
-						
-		String [] stringValueParts = stringValue.split(C.DIRECTORY_STATUS_DELIMITER, 2);
-		
-		String directoryStatus    = StringUtil.reverse(stringValueParts[0]);
-		String directoryPathValue = StringUtil.reverse(stringValueParts[1]);
+		Status directoryStatus = ((ImageRepositoryItem)value).getPathStatus();
+		String directoryPathValue = ((ImageRepositoryItem)value).getPath();
 		
 		this.setText(directoryPathValue);
 		
-		if (directoryStatus.equals(Status.DOES_NOT_EXIST.toString())) {
+		switch (directoryStatus) {
+		case DOES_NOT_EXIST:
 			this.setBackground(new Color(255,127,127));
 			this.setToolTipText("Image Repository path does not exist.");
-		} else if (directoryStatus.equals(Status.EXISTS.toString())) {
+			break;
+		case EXISTS:
 			this.setBackground(new Color(127,255,127));
 			this.setToolTipText("Image Repository path exists.");
-		} else if (directoryStatus.equals(Status.NOT_AVAILABLE.toString())) {
+			break;
+		case NOT_AVAILABLE:
 			this.setBackground(new Color(251,231,128));
 			this.setToolTipText("Image Repository is not available.");
+			break;
 		}
 		
 		if(isSelected) {
