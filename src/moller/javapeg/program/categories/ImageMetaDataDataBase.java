eb476d91c0be540d12f9d303a919846f37c122ff
fredrik_m <afmoller@users.noreply.github.com>
2010-02-06 11:25:14 +0000

    Renamed class.

--- src/moller/javapeg/program/categories/ImageMetaDataDataBase.java
+++ src/moller/javapeg/program/categories/ImageMetaDataDataBase.java
@@ -2,9 +2,13 @@ package moller.javapeg.program.categories;
 
 import java.io.File;
 
-public class ImageTagHandler {
+public class ImageMetaDataDataBase {
 
 	public static void createTagsFileForImages(File directory) {
 		System.out.println(directory.getAbsolutePath());
+	}
+	
+	public static boolean create() {
+		return true;
 	}
 }
