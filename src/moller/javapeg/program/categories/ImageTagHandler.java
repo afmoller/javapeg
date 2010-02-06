751ff8cb99d8da6455bc26241b7172da100ad598
fredrik_m <afmoller@users.noreply.github.com>
2010-02-06 10:15:22 +0000

    Added an ImageTagHandler.

---
+++ src/moller/javapeg/program/categories/ImageTagHandler.java
@@ -0,0 +1,10 @@
+package moller.javapeg.program.categories;
+
+import java.io.File;
+
+public class ImageTagHandler {
+
+	public static void createTagsFileForImages(File directory) {
+		System.out.println(directory.getAbsolutePath());
+	}
+}
