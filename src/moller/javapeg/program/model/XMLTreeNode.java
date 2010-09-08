839ab646f6e5801df5f5f3ff8cb351f256e9fa6d
fredrik_m <afmoller@users.noreply.github.com>
2010-09-08 09:59:41 +0000

    Remove method, for removing nodes in a tree was added.

--- src/moller/javapeg/program/model/XMLTreeNode.java
+++ src/moller/javapeg/program/model/XMLTreeNode.java
@@ -1,10 +1,12 @@
 package moller.javapeg.program.model;
 
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
 
-public class XMLTreeNode {
+public class XMLTreeNode extends Object {
+
 	Element element;
 	public XMLTreeNode(Element element) {
 		this.element = element;
@@ -31,5 +33,11 @@ public class XMLTreeNode {
 	
 	public void setAttribute(String name, String value) {
 		element.setAttribute(name, value);
+	}
+	
+	public void remove() {
+		Node parent = element.getParentNode();
+		parent.removeChild(element);
+		
 	}
 }
