6ff40da36b1b7bb9c7a30cf6b63cc33c8de56538
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2010-09-23 20:13:37 +0000

    Implemented that categories are stored in alphabetical order when they are created and that they will be reordered if they are renamed and a reordering is required.

--- trunk/src/moller/util/gui/TreeUtil.java
+++ trunk/src/moller/util/gui/TreeUtil.java
@@ -1,9 +1,16 @@
 package moller.util.gui;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 
 public class TreeUtil {
@@ -44,6 +51,59 @@ public class TreeUtil {
 		
 		if (collapseRoot) {
 			tree.collapsePath(new TreePath(root.getPath()));
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void insertNodeInAlphabeticalOrder(DefaultMutableTreeNode childNode, DefaultMutableTreeNode parentNode, DefaultTreeModel model) {
+		Enumeration<DefaultMutableTreeNode> children = parentNode.children();
+		
+		String nodeName = childNode.toString();
+		int index = 0;
+		
+		if (children.hasMoreElements()) {
+			while (children.hasMoreElements()) {
+				String displayString = children.nextElement().toString();
+				if (nodeName.compareToIgnoreCase(displayString) < 1) {
+					break;
+				}
+				index++;
+			}
+			model.insertNodeInto(childNode, parentNode, index);
+		} else {
+			model.insertNodeInto(childNode, parentNode, 0);
+		}
+	}
+	
+	public static void sortNodesAlphabetically(DefaultMutableTreeNode nodeToSort, DefaultTreeModel model) {
+		int nrOfChildren = nodeToSort.getChildCount();
+		
+		if (nrOfChildren > 1) {
+			Map<String, DefaultMutableTreeNode> nameNodeMap = new HashMap<String, DefaultMutableTreeNode>(nrOfChildren);
+			
+			for (int i = 0; i < nrOfChildren; i++) {
+				DefaultMutableTreeNode child = (DefaultMutableTreeNode)nodeToSort.getChildAt(i);
+				nameNodeMap.put(child.toString(), child);
+			}
+
+			List<String> names = new ArrayList<String>(nameNodeMap.keySet());
+			
+			Collections.sort(names, new IgnoreCaseComparator());
+			
+			for (DefaultMutableTreeNode node : nameNodeMap.values()) {
+				model.removeNodeFromParent(node);	
+			}
+			
+			for (int i = 0; i < names.size(); i++) {
+				model.insertNodeInto(nameNodeMap.get(names.get(i)), nodeToSort, i);
+			}
+			model.reload();
+		}
+	}
+	
+	private static class IgnoreCaseComparator implements Comparator<String> {
+		public int compare(String o1, String o2) {
+			return o1.compareToIgnoreCase(o2);
 		}
 	}
 }
