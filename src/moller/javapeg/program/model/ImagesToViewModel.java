73a07d852b763e82899f8b16a55db01623d3435e
fredrik_m <afmoller@users.noreply.github.com>
2010-11-02 21:06:27 +0000

    New model added.

---
+++ src/moller/javapeg/program/model/ImagesToViewModel.java
@@ -0,0 +1,12 @@
+package moller.javapeg.program.model;
+
+import javax.swing.DefaultListModel;
+
+public class ImagesToViewModel extends DefaultListModel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
