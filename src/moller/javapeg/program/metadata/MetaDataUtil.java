3c5f38578ca9df402a35d9c6c4cf776b7183669d
fredrik_m <afmoller@users.noreply.github.com>
2010-11-02 21:04:15 +0000

    Major rewrite

--- src/moller/javapeg/program/metadata/MetaDataUtil.java
+++ src/moller/javapeg/program/metadata/MetaDataUtil.java
@@ -1,15 +1,7 @@
 package moller.javapeg.program.metadata;
-/**
- * This class was created : 2009-03-14 by Fredrik Möller
- * Latest changed         : 2009-09-13 by Fredrik Möller
- *                        : 2009-09-14 by Fredrik Möller
- */
 
 import java.io.File;
-import java.util.List;
-import java.util.Vector;
 
-import moller.javapeg.program.contexts.ApplicationContext;
 import moller.javapeg.program.language.Language;
 
 public class MetaDataUtil {
@@ -19,18 +11,20 @@ public class MetaDataUtil {
 		Language lang = Language.getInstance();
 		
 		MetaData md = MetaDataRetriever.getMetaData(jpgFile);
+		
+		String noValue = "no value";
 
 		return "<html>" +
 		         "<table>" +
 		           createTableRow("Filnamn", jpgFile.getName()) +
-		           createTableRow(lang.get("variable.pictureDate"), md.getExifDateAsString()) +
-		           createTableRow(lang.get("variable.pictureTime"), md.getExifTimeAsString()) +
-		           createTableRow(lang.get("variable.cameraModel"), md.getExifCameraModel()) +
-		           createTableRow(lang.get("variable.shutterSpeed"), md.getExifShutterSpeed().toString()) +
-		           createTableRow(lang.get("variable.isoValue"), Integer.toString(md.getExifISOValue())) +
-		           createTableRow(lang.get("variable.pictureWidth"), Integer.toString(md.getExifPictureWidth())) +
-		           createTableRow(lang.get("variable.pictureHeight"), Integer.toString(md.getExifPictureHeight())) +
-		           createTableRow(lang.get("variable.apertureValue"), md.getExifApertureValue()) +
+		           createTableRow(lang.get("variable.pictureDate"), MetaDataUtil.hasValue(md.getExifDateAsString()) ? md.getExifDateAsString() : noValue) +
+		           createTableRow(lang.get("variable.pictureTime"), MetaDataUtil.hasValue(md.getExifTimeAsString()) ? md.getExifTimeAsString() : noValue) +
+		           createTableRow(lang.get("variable.cameraModel"), MetaDataUtil.hasValue(md.getExifCameraModel()) ? md.getExifCameraModel() : noValue) +
+		           createTableRow(lang.get("variable.shutterSpeed"), MetaDataUtil.hasValue(md.getExifShutterSpeed().toString()) ? md.getExifShutterSpeed().toString() : noValue) +
+		           createTableRow(lang.get("variable.isoValue"), MetaDataUtil.hasValue(md.getExifISOValue()) ? Integer.toString(md.getExifISOValue()) : noValue) +
+		           createTableRow(lang.get("variable.pictureWidth"), MetaDataUtil.hasValue(md.getExifPictureWidth()) ? Integer.toString(md.getExifPictureWidth()) : noValue) +
+		           createTableRow(lang.get("variable.pictureHeight"), MetaDataUtil.hasValue(md.getExifPictureHeight()) ? Integer.toString(md.getExifPictureHeight()) : noValue) +
+		           createTableRow(lang.get("variable.apertureValue"), MetaDataUtil.hasValue(md.getExifApertureValue()) ? md.getExifApertureValue() : noValue) +
 		         "</table>" +
 			   "</html>";
 	}
@@ -42,27 +36,11 @@ public class MetaDataUtil {
 	           "</tr>";
 	}
 	
-	public static Vector<Vector<String>> getMetaData() {
-
-		Vector<Vector<String>> metaDataVector = new Vector<Vector<String>>();
-
-		List<MetaData> metaDataObjects = ApplicationContext.getInstance().getMetaDataObjects();			
-				
-		for (MetaData metaData : metaDataObjects) {
-			Vector<String> temp= new Vector<String>();
-			
-			temp.addElement(metaData.getFileName());
-			temp.addElement(metaData.getExifDateAsString());
-			temp.addElement(metaData.getExifTimeAsString());
-			temp.addElement(metaData.getExifCameraModel());
-			temp.addElement(metaData.getExifShutterSpeed().toString());
-			temp.addElement(Integer.toString(metaData.getExifISOValue()));
-			temp.addElement(Integer.toString(metaData.getExifPictureWidth()));
-			temp.addElement(Integer.toString(metaData.getExifPictureHeight()));
-			temp.addElement(metaData.getExifApertureValue());
-
-			metaDataVector.addElement(temp);
+	public static boolean hasValue(Object object) {
+		if (object instanceof Integer) {
+			return (Integer)object > -1;
+		} else {
+			return object != null;
 		}
-		return metaDataVector;
 	}
 }
