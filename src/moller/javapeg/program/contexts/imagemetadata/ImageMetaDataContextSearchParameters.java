6715cebf82b1c5224d5bff28d2344f10f30312df
fredrik_m <afmoller@users.noreply.github.com>
2010-11-11 21:30:39 +0000

    More image meta data context search parameters was added.

--- src/moller/javapeg/program/contexts/imagemetadata/ImageMetaDataContextSearchParameters.java
+++ src/moller/javapeg/program/contexts/imagemetadata/ImageMetaDataContextSearchParameters.java
@@ -1,18 +1,64 @@
 package moller.javapeg.program.contexts.imagemetadata;
 
 import moller.javapeg.program.categories.Categories;
+import moller.javapeg.program.logger.Logger;
+import moller.util.datatype.ShutterSpeed;
 
 public class ImageMetaDataContextSearchParameters {
 	
+	private static Logger logger = Logger.getInstance();
+	
+	private String cameraModel;
+	private String comment;
 	private Categories categories;
+	private int iso;
+	private ShutterSpeed shutterSpeed;
+	private boolean[] ratings;
 	
 	public ImageMetaDataContextSearchParameters() {
 		super();
+		this.cameraModel = null;
+		this.comment = null;
 		this.categories = null;
+		this.iso = -1;
+		this.shutterSpeed = null;
+		this.ratings = null;
+	}
+	
+	public String getCameraModel() {
+		return cameraModel;
+	}
+	
+	public String getComment() {
+		return comment;
 	}
 
 	public Categories getCategories() {
 		return categories;
+	}
+	
+	public int getIso() {
+		return iso;
+	}
+	
+	public boolean[] getRatings() {
+		return ratings;
+	}
+	
+	public ShutterSpeed getShutterSpeed() {
+		return shutterSpeed;
+	}
+
+	public void setCameraModel(String cameraModel) {
+		if (!cameraModel.equals("")) {
+			this.cameraModel = cameraModel;	
+		}
+	}
+	
+	public void setComment(String comment) {
+		if (!comment.equals("")) {
+			this.comment = comment;	
+		}
 	}
 
 	public void setCategories(Categories categories) {
@@ -20,4 +66,24 @@ public class ImageMetaDataContextSearchParameters {
 			this.categories = categories;
 		}
 	}
+	
+	public void setIso(String iso) {
+		if (!iso.equals("")) {
+			try {
+				this.iso = Integer.parseInt(iso);	
+			} catch (NumberFormatException nfex) {
+				logger.logERROR("Could not parse String " + iso + " to an integer, see stacktrace below for details");
+				logger.logERROR(nfex);
+				this.iso  = -1;
+			}
+		}
+	}
+	
+	public void setRating(boolean[] ratings) {
+		this.ratings = ratings;
+	}
+	
+	public void setShutterSpeed(ShutterSpeed shutterSpeed) {
+		this.shutterSpeed = shutterSpeed;
+	}	
 }
