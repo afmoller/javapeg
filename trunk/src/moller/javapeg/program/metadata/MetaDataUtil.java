c2a1b22cde38e2536230df144659a57f26d6fbe5
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2010-11-16 10:15:25 +0000

    Implemented the possibility to set the tooltip levels for thumbnail: disabled, enabled and extended.

--- trunk/src/moller/javapeg/program/metadata/MetaDataUtil.java
+++ trunk/src/moller/javapeg/program/metadata/MetaDataUtil.java
@@ -2,6 +2,7 @@ package moller.javapeg.program.metadata;
 
 import java.io.File;
 
+import moller.javapeg.program.config.Config;
 import moller.javapeg.program.language.Language;
 
 public class MetaDataUtil {
@@ -13,20 +14,26 @@ public class MetaDataUtil {
 		MetaData md = MetaDataRetriever.getMetaData(jpgFile);
 		
 		String noValue = "no value";
-
-		return "<html>" +
-		         "<table>" +
-		           createTableRow("Filnamn", jpgFile.getName()) +
-		           createTableRow(lang.get("variable.pictureDate"), MetaDataUtil.hasValue(md.getExifDateAsString()) ? md.getExifDateAsString() : noValue) +
-		           createTableRow(lang.get("variable.pictureTime"), MetaDataUtil.hasValue(md.getExifTimeAsString()) ? md.getExifTimeAsString() : noValue) +
-		           createTableRow(lang.get("variable.cameraModel"), MetaDataUtil.hasValue(md.getExifCameraModel()) ? md.getExifCameraModel() : noValue) +
-		           createTableRow(lang.get("variable.shutterSpeed"), MetaDataUtil.hasValue(md.getExifShutterSpeed().toString()) ? md.getExifShutterSpeed().toString() : noValue) +
-		           createTableRow(lang.get("variable.isoValue"), MetaDataUtil.hasValue(md.getExifISOValue()) ? Integer.toString(md.getExifISOValue()) : noValue) +
-		           createTableRow(lang.get("variable.pictureWidth"), MetaDataUtil.hasValue(md.getExifPictureWidth()) ? Integer.toString(md.getExifPictureWidth()) : noValue) +
-		           createTableRow(lang.get("variable.pictureHeight"), MetaDataUtil.hasValue(md.getExifPictureHeight()) ? Integer.toString(md.getExifPictureHeight()) : noValue) +
-		           createTableRow(lang.get("variable.apertureValue"), MetaDataUtil.hasValue(md.getExifApertureValue()) ? md.getExifApertureValue() : noValue) +
-		         "</table>" +
-			   "</html>";
+		
+		if (Config.getInstance().getStringProperty("thumbnails.tooltip.state").equals("2")) {
+			return "<html>" +
+	         "<table>" +
+//	         TODO: Fix this bug
+	           createTableRow("Filnamn", jpgFile.getName()) +
+	           createTableRow(lang.get("variable.pictureDate"), MetaDataUtil.hasValue(md.getExifDateAsString()) ? md.getExifDateAsString() : noValue) +
+	           createTableRow(lang.get("variable.pictureTime"), MetaDataUtil.hasValue(md.getExifTimeAsString()) ? md.getExifTimeAsString() : noValue) +
+	           createTableRow(lang.get("variable.cameraModel"), MetaDataUtil.hasValue(md.getExifCameraModel()) ? md.getExifCameraModel() : noValue) +
+	           createTableRow(lang.get("variable.shutterSpeed"), MetaDataUtil.hasValue(md.getExifShutterSpeed().toString()) ? md.getExifShutterSpeed().toString() : noValue) +
+	           createTableRow(lang.get("variable.isoValue"), MetaDataUtil.hasValue(md.getExifISOValue()) ? Integer.toString(md.getExifISOValue()) : noValue) +
+	           createTableRow(lang.get("variable.pictureWidth"), MetaDataUtil.hasValue(md.getExifPictureWidth()) ? Integer.toString(md.getExifPictureWidth()) : noValue) +
+	           createTableRow(lang.get("variable.pictureHeight"), MetaDataUtil.hasValue(md.getExifPictureHeight()) ? Integer.toString(md.getExifPictureHeight()) : noValue) +
+	           createTableRow(lang.get("variable.apertureValue"), MetaDataUtil.hasValue(md.getExifApertureValue()) ? md.getExifApertureValue() : noValue) +
+	         "</table>" +
+		   "</html>";
+		} else {
+//			TODO: Fix hard coded string
+			return "File name: " + jpgFile.getName(); 
+		}
 	}
 	
 	private static String createTableRow(String metaDataKey, String metaDataValue) {
