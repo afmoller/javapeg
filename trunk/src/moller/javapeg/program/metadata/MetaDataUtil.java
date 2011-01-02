bcd0defc831bcaf1c8bd91d7e8606c8d6e4bdcd4
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2011-01-02 14:44:30 +0000

    The parseImageFile was moved.

--- trunk/src/moller/javapeg/program/metadata/MetaDataUtil.java
+++ trunk/src/moller/javapeg/program/metadata/MetaDataUtil.java
@@ -1,6 +1,17 @@
 package moller.javapeg.program.metadata;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.drew.imaging.jpeg.JpegMetadataReader;
+import com.drew.imaging.jpeg.JpegProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.MetadataException;
+import com.drew.metadata.Tag;
+import com.drew.metadata.exif.ExifDirectory;
 
 import moller.javapeg.program.config.Config;
 import moller.javapeg.program.language.Language;
@@ -49,5 +60,68 @@ public class MetaDataUtil {
 		} else {
 			return object != null;
 		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static Map<String, String> parseImageFile(File imageFile) {
+		Map<String, String> tagsMap = new HashMap<String, String>();
+		
+		try{
+			Metadata metadata = JpegMetadataReader.readMetadata(imageFile);
+			Iterator directories = metadata.getDirectoryIterator();
+
+			while (directories.hasNext()) {
+				Directory directory = (Directory)directories.next();
+				Iterator tags = directory.getTagIterator();
+
+				while (tags.hasNext()) {
+					Tag tag = (Tag)tags.next();
+					
+					if(tag.toString().indexOf("Unknown tag") == -1){
+						try {
+							tagsMap.put(tag.getTagTypeHex(), tag.getDescription());
+						} catch (MetadataException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+				if (directory.hasErrors()) {
+					Iterator errors = directory.getErrors();
+					while (errors.hasNext()) {
+//						TODO: Add logging
+						System.out.println("ERROR: " + errors.next());
+					}
+				}
+			}
+		} catch (JpegProcessingException jpe) {
+			System.err.println("error 1a");
+			jpe.printStackTrace();
+		}
+		return tagsMap;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static int getOrientationTag(File imageFile) {
+		
+		try{
+			Metadata metadata = JpegMetadataReader.readMetadata(imageFile);
+			Iterator directories = metadata.getDirectoryIterator();
+
+			while (directories.hasNext()) {
+				Directory directory = (Directory)directories.next();
+		
+				if(directory.containsTag(ExifDirectory.TAG_ORIENTATION)) {
+					return directory.getInt(ExifDirectory.TAG_ORIENTATION);
+				}
+			}
+		} catch (JpegProcessingException jpe) {
+			System.err.println("error 1a");
+			jpe.printStackTrace();
+		} catch (MetadataException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return -1;
 	}
 }
