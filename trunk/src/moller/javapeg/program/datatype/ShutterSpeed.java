450330362579dbf0a46ecbf526d82698e9cc0938
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2011-03-29 20:12:24 +0000

    ShutterSpeed handling improved. Decimal format shutter speeds, such as "0.5" is handled correctly now.

--- trunk/src/moller/javapeg/program/datatype/ShutterSpeed.java
+++ trunk/src/moller/javapeg/program/datatype/ShutterSpeed.java
@@ -7,97 +7,89 @@ public class ShutterSpeed implements Comparable<ShutterSpeed> {
 
 	private int seconds;
 	private Rational partsOfSecond;
-	
-	public ShutterSpeed(int seconds) {
-		this.seconds = seconds;
-		this.partsOfSecond = null;
-	}
-	
-	public ShutterSpeed(Rational partsOfSecond) {
-		this.seconds = -1;
-		this.partsOfSecond = partsOfSecond;
-	}
-	
-	public ShutterSpeed(int seconds, Rational partsOfSecond) {
-		this.seconds = seconds;
-		this.partsOfSecond = partsOfSecond;
-	}
-	
+	private float shutterSpeedAsFloat;
+
 	public ShutterSpeed(String shutterSpeed) throws ShutterSpeedException {
 		shutterSpeed = StringUtil.removeAnyPrecedingAndTrailingNonIntegerCharacters(shutterSpeed);
-		
+
 		if (shutterSpeed.contains(Rational.DELIMITER)) {
 			shutterSpeed = shutterSpeed.trim();
 			int delimiterIndex = -1;
-			
+
 			if (shutterSpeed.contains(" ")) {
 				String[] parts = shutterSpeed.split(" ");
 				if (parts.length != 2) {
-					throw new ShutterSpeedException("Invalid format of ShutterSpeed: " + shutterSpeed); 
+					throw new ShutterSpeedException("Invalid format of ShutterSpeed: " + shutterSpeed);
 				}
-				
+
 				String seconds        = parts[0];
 				String partsOfSeconds = parts[1];
-				
+
 				delimiterIndex = partsOfSeconds.indexOf(Rational.DELIMITER);
 				this.seconds = Integer.parseInt(seconds);
 				this.partsOfSecond = new Rational(partsOfSeconds.substring(0, delimiterIndex), partsOfSeconds.substring(delimiterIndex + 1, partsOfSeconds.length()));
-				
+				this.shutterSpeedAsFloat = Float.MIN_VALUE;
+
 			} else {
 				delimiterIndex = shutterSpeed.indexOf(Rational.DELIMITER);
 				this.seconds = -1;
 				this.partsOfSecond = new Rational(shutterSpeed.substring(0, delimiterIndex), shutterSpeed.substring(delimiterIndex + 1, shutterSpeed.length()));
+				this.shutterSpeedAsFloat = Float.MIN_VALUE;
+			}
+		} else if (shutterSpeed.contains(".") || shutterSpeed.contains(",")) {
+			if (shutterSpeed.contains(",")) {
+				shutterSpeed = shutterSpeed.replace(",", ".");
+			}
+			try {
+				this.seconds = -1;
+				this.partsOfSecond = null;
+				this.shutterSpeedAsFloat  = Float.parseFloat(shutterSpeed);
+			} catch (NumberFormatException nfex) {
+				throw new ShutterSpeedException("Invalid format of ShutterSpeed: " + shutterSpeed);
 			}
 		} else {
 			try {
 				this.seconds = Integer.parseInt(shutterSpeed.trim());
+				this.partsOfSecond = null;
+				this.shutterSpeedAsFloat = Float.MIN_VALUE;
 			} catch (NumberFormatException nfex) {
 				throw new ShutterSpeedException("Invalid format of ShutterSpeed: " + shutterSpeed);
 			}
 		}
 	}
 
-	public int getSeconds() {
-		return seconds;
-	}
-
-	public Rational getPartsOfSecond() {
-		return partsOfSecond;
-	}
-
-	public void setSeconds(int seconds) {
-		this.seconds = seconds;
-	}
-
-	public void setPartsOfSecond(Rational partsOfSecond) {
-		this.partsOfSecond = partsOfSecond;
-	}
-	
+	@Override
 	public String toString() {
-		if (partsOfSecond == null && seconds == -1) {
+		if (partsOfSecond == null && seconds == -1 && shutterSpeedAsFloat == Float.MIN_VALUE) {
 			return "";
-		} else if (partsOfSecond == null) {
+		} else if (shutterSpeedAsFloat == Float.MIN_VALUE && partsOfSecond == null) {
 			return Integer.toString(seconds);
-		} else if (seconds == -1){
+		} else if (shutterSpeedAsFloat == Float.MIN_VALUE && seconds == -1){
 			return partsOfSecond.toString();
+		} else if (partsOfSecond == null && seconds == -1){
+			return Float.toString(shutterSpeedAsFloat);
 		} else {
 			return Integer.toString(seconds) + " " + partsOfSecond.toString();
 		}
 	}
-	
+
 	private float comparableValue() {
 		float result = 0;
-		
+
 		if (seconds > 0) {
 			result += seconds;
-		} 
-		
+		}
+
 		if (partsOfSecond != null) {
 			result += (float)partsOfSecond.getNumerator() / (float)partsOfSecond.getDenominator();
-		} 
+		}
+
+		if (shutterSpeedAsFloat != Float.MIN_NORMAL) {
+			result += shutterSpeedAsFloat;
+		}
 		return result;
 	}
-	
+
 	public class ShutterSpeedException extends Exception {
 		private static final long serialVersionUID = 1L;
 
