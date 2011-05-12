aa95deb5914207879a8ef33f6e209760c17dde5e
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2011-05-12 19:43:47 +0000

    Fixed better labels for the different modes of directory additions to the image repository.

--- trunk/src/moller/javapeg/resources/lang/languages/sv/configviewer.sv
+++ trunk/src/moller/javapeg/resources/lang/languages/sv/configviewer.sv
@@ -101,9 +101,9 @@ configviewer.tag.imageRepositories.label.exists                         = Existe
 configviewer.tag.imageRepositories.label.notAvailable                   = Inte Tillgänglig
 configviewer.tag.imageRepositories.label.doesNotExist                   = Existerar inte
 configviewer.tag.imageRepositories.label.pathsWillBeRemoved             = Följande sökväg(ar) kommer att tas bort från bilddatabasen:
-configviewer.tag.imageRepositories.label.addAutomatically               = Lägg till automatiskt
-configviewer.tag.imageRepositories.label.askToAdd                       = Fråga för att lägga till
-configviewer.tag.imageRepositories.label.doNotAskToAdd                  = Fråga inte
+configviewer.tag.imageRepositories.label.addAutomatically               = Lägg automatiskt till en katalog med icke tillagda bilder till bilddatabasen
+configviewer.tag.imageRepositories.label.askToAdd                       = Fråga för att lägga till katalog med icke tillagda bilder till bilddatabasen 
+configviewer.tag.imageRepositories.label.doNotAskToAdd                  = Lägg inte till en katalog med icke tillagda bilder till bilddatabasen
 
 ##############################################################################################################
 # E R R O R  M E S S A G E S                                                                                 #
