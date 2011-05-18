1a03785c3e8fe716cfe541cf8d5bb315d4497765
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2011-05-18 19:00:25 +0000

    Code formatted.

--- trunk/src/moller/javapeg/program/imagerepository/ImageRepositoryItem.java
+++ trunk/src/moller/javapeg/program/imagerepository/ImageRepositoryItem.java
@@ -1,39 +1,39 @@
 package moller.javapeg.program.imagerepository;
-/**
- * This class was created : 2010-01-21 by Fredrik Möller
- * Latest changed         : 2010-01-28 by Fredrik Möller
- *                        : 2010-02-06 by Fredrik Möller
- */
 
 import moller.util.io.Status;
 
 public class ImageRepositoryItem implements Comparable<ImageRepositoryItem>{
-	
+
 	private String path;
 	private Status pathStatus;
-	
+
 	public ImageRepositoryItem() {
 		this.path = null;
 		this.pathStatus = null;
 	}
-		
+
 	public ImageRepositoryItem(String path, Status pathStatus) {
 		super();
 		this.path = path;
 		this.pathStatus = pathStatus;
 	}
+
 	public String getPath() {
 		return path;
 	}
+
 	public Status getPathStatus() {
 		return pathStatus;
 	}
+
 	public void setPath(String path) {
 		this.path = path;
 	}
+
 	public void setPathStatus(Status pathStatus) {
 		this.pathStatus = pathStatus;
 	}
+
 	public void setPathStatus(String pathStatus) {
 		if(pathStatus.equals("EXISTS")) {
 			this.pathStatus = Status.EXISTS;
@@ -41,9 +41,9 @@ public class ImageRepositoryItem implements Comparable<ImageRepositoryItem>{
 			this.pathStatus = Status.NOT_AVAILABLE;
 		} else if(pathStatus.equals("DOES_NOT_EXIST")) {
 			this.pathStatus = Status.DOES_NOT_EXIST;
-		} 	
+		}
 	}
-	
+
 	@Override
 	public int compareTo(ImageRepositoryItem iri) {
 		return this.path.compareTo(iri.getPath());
