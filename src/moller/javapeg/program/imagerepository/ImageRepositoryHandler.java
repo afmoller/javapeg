6cb4a8ab662e74aa308b892d55d522e7785d186e
fredrik_m <afmoller@users.noreply.github.com>
2011-05-18 19:08:36 +0000

    The image repository file is only updated if changes have been done to the content of the image repository; adding or removal of directories. Additional logging has been made as well.

--- src/moller/javapeg/program/imagerepository/ImageRepositoryHandler.java
+++ src/moller/javapeg/program/imagerepository/ImageRepositoryHandler.java
@@ -20,6 +20,8 @@ public class ImageRepositoryHandler {
 
 	private final File repositoryFile;
 
+	private TreeSet<String> imageRepositoryPaths;
+
 	/**
 	 * Private constructor.
 	 */
@@ -47,7 +49,8 @@ public class ImageRepositoryHandler {
 		Logger logger = Logger.getInstance();
 		if(repositoryFile.exists()) {
 			try {
-				return new TreeSet<String>(FileUtil.readFromFile(repositoryFile)).toArray();
+				imageRepositoryPaths = new TreeSet<String>(FileUtil.readFromFile(repositoryFile));
+				return imageRepositoryPaths.toArray();
 			} catch (IOException iox) {
 				logger.logERROR("Can not read from file: " + repositoryFile.getAbsolutePath());
 				logger.logERROR(iox);
@@ -67,10 +70,70 @@ public class ImageRepositoryHandler {
 				directoryPaths.add(((ImageRepositoryItem)imageRepositoryItems.next()).getPath());
 			}
 
-			FileUtil.writeToFile(repositoryFile, directoryPaths, false);
+			if (contentChanged(directoryPaths)) {
+				FileUtil.writeToFile(repositoryFile, directoryPaths, false);
+				logger.logDEBUG("Image Repository file: " + repositoryFile.getAbsolutePath() + " updated");
+
+				for (String addedPath : addedImageRepositoryPaths(directoryPaths)) {
+					logger.logDEBUG("Image Repository Path Added: " + addedPath);
+				}
+
+				for (String removedPath : removedImageRepositoryPaths(directoryPaths)) {
+					logger.logDEBUG("Image Repository Path Removed: " + removedPath);
+				}
+			} else {
+				logger.logDEBUG("Image Repository file: " + repositoryFile.getAbsolutePath() + " not updated since there was no changes found");
+			}
 		} catch (IOException iox) {
 			logger.logERROR("Can not write to file: " + repositoryFile.getAbsolutePath());
 			logger.logERROR(iox);
 		}
+	}
+
+	/**
+	 * This method decides whether the image repository has changed while
+	 * JavaPEG has been running. If a change is found then the image repository
+	 * file needs to be updated by flushing the current content to persistent
+	 * storage.
+	 *
+	 * @param directoryPaths contains the current image repository paths.
+	 *
+	 * @return a boolean value indicating whether the image repository file
+	 *         need to be updated or not.
+	 */
+	private boolean contentChanged(List<String> directoryPaths) {
+
+		if (directoryPaths.size() != imageRepositoryPaths.size()) {
+			return true;
+		}
+
+		for (String imageRepositoryPath : imageRepositoryPaths) {
+			if (!directoryPaths.contains(imageRepositoryPath)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private List<String> addedImageRepositoryPaths(List<String> directoryPaths) {
+		List<String> addedPaths = new ArrayList<String>();
+
+		for (String directoryPath : directoryPaths) {
+			if (!imageRepositoryPaths.contains(directoryPath)) {
+				addedPaths.add(directoryPath);
+			}
+		}
+		return addedPaths;
+	}
+
+	private List<String> removedImageRepositoryPaths(List<String> directoryPaths) {
+		List<String> removedPaths = new ArrayList<String>();
+
+		for (String imageRepositoryPath : imageRepositoryPaths) {
+			if (!directoryPaths.contains(imageRepositoryPath)) {
+				removedPaths.add(imageRepositoryPath);
+			}
+		}
+		return removedPaths;
 	}
 }
