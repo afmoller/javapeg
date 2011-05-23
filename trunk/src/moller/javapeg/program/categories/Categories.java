bbf4f3abff053ea935297eb3b6ce42895059cc8e
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2011-05-23 20:26:11 +0000

    Implemented a write-only-if-needed functionality for the image meta data files. In other words if none of the comments, ratings or categories for any of the viewed images in JavaPEG has been changed, then there is no need to flush the file data to disk as it has been until now. This improvement, increases the loading speed when a new directory is to be displayed and decreases unnecessary file access.

--- trunk/src/moller/javapeg/program/categories/Categories.java
+++ trunk/src/moller/javapeg/program/categories/Categories.java
@@ -6,13 +6,13 @@ import java.util.TreeSet;
 import moller.util.string.StringUtil;
 
 /**
- * This class 
- * 
+ * This class
+ *
  * @author Fredrik
  *
  */
 public class Categories {
-	
+
 	private static final long serialVersionUID = 1L;
 	private Set<String> categories;
 
@@ -20,7 +20,7 @@ public class Categories {
 		super();
 		this.categories = new TreeSet<String>();
 	}
-	
+
 	public Categories(String[] categories) {
 		super();
 		this.categories = new TreeSet<String>();
@@ -34,15 +34,15 @@ public class Categories {
 		String[] categories = categoriesString.split(",");
 		for (String category : categories) {
 			if (StringUtil.isInt(category)) {
-				addCategory(category);	
+				addCategory(category);
 			}
 		}
 	}
-	
+
 	public void addCategory(String category) {
 		this.categories.add(category);
 	}
-	
+
 	public Set<String> getCategories() {
 		return categories;
 	}
@@ -50,11 +50,11 @@ public class Categories {
 	public void setCategories(Set<String> categories) {
 		this.categories = categories;
 	}
-	
+
 	public int size() {
 		return categories.size();
 	}
-	
+
 	public boolean contains(String category) {
 		return categories.contains(category);
 	}
@@ -62,17 +62,31 @@ public class Categories {
 	@Override
 	public String toString() {
 		String categoryString= null;
-		
+
 		if (categories != null && categories.size() > 0) {
 			StringBuilder sb = new StringBuilder();
-	    	
+
 			for (String category : categories) {
 				sb.append(category);
-				sb.append(",");	
+				sb.append(",");
 			}
 	    	categoryString = sb.toString();
-	    	categoryString = StringUtil.removeLastCharacter(categoryString);	
+	    	categoryString = StringUtil.removeLastCharacter(categoryString);
 		}
 		return categoryString;
+	}
+
+	@Override
+	public boolean equals(Object anObject) {
+	    if (this == anObject) {
+	        return true;
+	    }
+	    if (anObject instanceof Categories) {
+	        Categories anotherCategories = (Categories)anObject;
+	        if (this.getCategories().equals(anotherCategories.getCategories())) {
+	            return true;
+	        }
+	    }
+	    return false;
 	}
 }
