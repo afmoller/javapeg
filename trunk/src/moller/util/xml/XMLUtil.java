3e56734943b993ed4f0829397800dfb418c48a5f
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2011-10-04 18:26:00 +0000

    Added an encoding parameter to the writeStartDocument method.

--- trunk/src/moller/util/xml/XMLUtil.java
+++ trunk/src/moller/util/xml/XMLUtil.java
@@ -1,23 +1,20 @@
 package moller.util.xml;
-/**
- * This class was created : 2010-02-07 by Fredrik Möller
- * Latest changed         : 
- */
 
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 
 public class XMLUtil {
-		
+
 	/**
+	 * @param encoding
 	 * @param version
 	 * @param xmlsw
 	 * @throws XMLStreamException
 	 */
-	public static void writeStartDocument(String version, XMLStreamWriter xmlsw) throws XMLStreamException {
-		xmlsw.writeStartDocument(version);
+	public static void writeStartDocument(String encoding, String version, XMLStreamWriter xmlsw) throws XMLStreamException {
+		xmlsw.writeStartDocument(encoding, version);
 	}
-	
+
 	/**
 	 * @param element
 	 * @param value
@@ -27,7 +24,7 @@ public class XMLUtil {
 	public static void writeElement(String element, String value, XMLStreamWriter xmlsw) throws XMLStreamException {
 		writeElement(element, value, null, xmlsw);
 	}
-	
+
 	/**
 	 * @param element
 	 * @param value
@@ -38,12 +35,12 @@ public class XMLUtil {
 	public static void writeElement(String element, String value, XMLAttribute[] xmlAttributes, XMLStreamWriter xmlsw) throws XMLStreamException {
 		xmlsw.writeStartElement(element);
 		if (value != null) {
-			xmlsw.writeCharacters(value);	
+			xmlsw.writeCharacters(value);
 		}
-		
+
 		if (xmlAttributes != null) {
 			for (XMLAttribute xmlAttribute : xmlAttributes) {
-				xmlsw.writeAttribute(xmlAttribute.getName(), xmlAttribute.getValue());	
+				xmlsw.writeAttribute(xmlAttribute.getName(), xmlAttribute.getValue());
 			}
 		}
 		xmlsw.writeEndElement();
@@ -69,7 +66,7 @@ public class XMLUtil {
 		xmlsw.writeStartElement(element);
 		xmlsw.writeAttribute(attributeName, attributeValue);
 	}
-	
+
 	/**
 	 * @param element
 	 * @param xmlAttributes
