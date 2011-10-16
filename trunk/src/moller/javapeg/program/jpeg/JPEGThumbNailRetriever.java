db04f2f2038de834f21063cf2c03fd3f91c8e58e
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2011-10-16 14:55:56 +0000

    Changed the retrieveThumbnailFrom method to handle jpeg images without any fetchable meta data in them.

--- trunk/src/moller/javapeg/program/jpeg/JPEGThumbNailRetriever.java
+++ trunk/src/moller/javapeg/program/jpeg/JPEGThumbNailRetriever.java
@@ -61,78 +61,110 @@ public class JPEGThumbNailRetriever {
 		// ..and if the thumbnail was not existing in the cache or cache not enabled.
 		if (thumbNail == null) {
 			thumbNail = new JPEGThumbNail();
+			byte [] thumbNailData = null;
 
 			// Hämta in metadata för filen som tumnageln skall hämtas ur
 			MetaData md = MetaDataRetriever.getMetaData(jpegFile);
 
 			int thumbNailLength = md.getThumbNailLength();
 
-			// Skapa plats för bilden
-			byte [] thumbNailData = new byte[thumbNailLength];
+			if (thumbNailLength > 0) {
+			    try {
+			        // Skapa plats för bilden
+			        thumbNailData = new byte[thumbNailLength];
 
-			FileInputStream image = null;
+			        FileInputStream image = null;
 
-			try {
-				image = new FileInputStream(jpegFile);
+			        try {
+			            image = new FileInputStream(jpegFile);
 
-				try {
-					// +12 = jpgfilens header i bytes
-					long thumbNailOffsetLong = (long)md.getThumbNailOffset() + 12;
-					long skippedBytes = image.skip(thumbNailOffsetLong);
+			            try {
+			                // +12 = jpgfilens header i bytes
+			                long thumbNailOffsetLong = (long)md.getThumbNailOffset() + 12;
+			                long skippedBytes = image.skip(thumbNailOffsetLong);
 
-					// eftersom skip()-metoden enligt api:t kan skippa färre bytes
-					// än anigvina så kontrolleras detta här och korrigeras vid behov
-					// detta.
-					while(skippedBytes < thumbNailOffsetLong) {
-						thumbNailOffsetLong = thumbNailOffsetLong - skippedBytes;
-						skippedBytes = image.skip(thumbNailOffsetLong);
-					}
-					image.read(thumbNailData, 0, thumbNailLength);
+			                // eftersom skip()-metoden enligt api:t kan skippa färre bytes
+			                // än anigvina så kontrolleras detta här och korrigeras vid behov
+			                // detta.
+			                while(skippedBytes < thumbNailOffsetLong) {
+			                    thumbNailOffsetLong = thumbNailOffsetLong - skippedBytes;
+			                    skippedBytes = image.skip(thumbNailOffsetLong);
+			                }
+			                image.read(thumbNailData, 0, thumbNailLength);
+			            }  catch(IOException iox) {
+			                logger.logERROR("Could not retrieve the thumbnail for image: " + jpegFile.getAbsolutePath());
+			                logger.logERROR(iox);
+			            } finally {
+			                StreamUtil.close(image, true);
+			            }
+			        } catch(FileNotFoundException fnfex) {
+			            logger.logERROR("Could not open file: " + jpegFile + " for reading. See stacktrace below for details");
+			            logger.logERROR(fnfex);
+			        }
 
-					if (!JPEGUtil.isJPEG(thumbNailData)) {
-						logger.logDEBUG("Embedded thumbnail missing or corrupt in file: " + jpegFile.getAbsolutePath());
+			        if (!JPEGUtil.isJPEG(thumbNailData)) {
+			            logger.logDEBUG("Embedded thumbnail missing or corrupt in file: " + jpegFile.getAbsolutePath());
 
-						if(config.getBooleanProperty("thumbnails.view.create-if-missing-or-corrupt")) {
-							try {
-								logger.logDEBUG("Searching for thumbnail in image: "  + jpegFile.getAbsolutePath());
-								thumbNailData = JPEGUtil.searchForThumbnail(jpegFile);
-								logger.logDEBUG("Found thumbnail in image: "  + jpegFile.getAbsolutePath());
-							} catch (IOException iox) {
-								logger.logDEBUG("Creating thumbnail for image: "  + jpegFile.getAbsolutePath());
+			            if(config.getBooleanProperty("thumbnails.view.create-if-missing-or-corrupt")) {
+			                thumbNailData = searchForOrCreateThumbnail(jpegFile);
+			            } else {
+			                logger.logDEBUG("Loading missing thumbnail image");
+			                return new JPEGThumbNail(StreamUtil.getByteArray(StartJavaPEG.class.getResourceAsStream("resources/images/missing.png")));
+			            }
+			        }
 
-								String algorithm = config.getStringProperty("thumbnails.view.create.algorithm");
-								logger.logDEBUG("Thumbnail creation algorithm: " + algorithm);
-
-								thumbNailData = JPEGUtil.createThumbNail(jpegFile, config.getIntProperty("thumbnails.view.width"), config.getIntProperty("thumbnails.view.height"), algorithm.equals("FAST") ? JPEGScaleAlgorithm.FAST : JPEGScaleAlgorithm.SMOOTH);
-							}
-						} else {
-							logger.logDEBUG("Loading missing thumbnail image");
-							return new JPEGThumbNail(StreamUtil.getByteArray(StartJavaPEG.class.getResourceAsStream("resources/images/missing.png")));
-						}
-					}
-
-					thumbNail.setThumbNailData(thumbNailData);
-					thumbNail.setThumbNailSize(thumbNailLength);
-
-					if(jpgtnc != null && config.getBooleanProperty("thumbnails.cache.enabled")) {
-						jpgtnc.add(jpegFile, thumbNail);
-					}
-				} catch(IOException iox) {
-					logger.logERROR("Could not retrieve the thumbnail for image: " + jpegFile.getAbsolutePath());
-					logger.logERROR(iox);
-				} finally {
-					if (image != null) {
-						try {
-							image.close();
-						} catch (IOException e) {
-							logger.logERROR(e);
-						}
-					}
-				}
-			} catch(FileNotFoundException fnfex) {
-				logger.logERROR(fnfex);
+			    } catch (IOException iox) {
+			        logger.logERROR("Could not create the missing image, set stacktrace below for details");
+			        logger.logERROR(iox);
+                }
 			}
+			// Could not find the meta data for the thumbnail length
+            else {
+                if(config.getBooleanProperty("thumbnails.view.create-if-missing-or-corrupt")) {
+
+                    try {
+                        thumbNailData = searchForOrCreateThumbnail(jpegFile);
+                    } catch (IOException iox) {
+                        logger.logERROR("Could not find or create thumbnail for image: " + jpegFile.getAbsolutePath() + " see statcktrace below for details");
+                        logger.logERROR(iox);
+                    }
+                } else {
+                    logger.logDEBUG("Loading missing thumbnail image");
+                    try {
+                        return new JPEGThumbNail(StreamUtil.getByteArray(StartJavaPEG.class.getResourceAsStream("resources/images/missing.png")));
+                    } catch (IOException iox) {
+                       logger.logFATAL("Could not get internal image resource for \"missing image\". See stacktrace below for details");
+                       logger.logFATAL(iox);
+                    }
+                }
+            }
+
+            thumbNail.setThumbNailData(thumbNailData);
+            thumbNail.setThumbNailSize(thumbNailLength);
+
+            if(jpgtnc != null && config.getBooleanProperty("thumbnails.cache.enabled")) {
+                jpgtnc.add(jpegFile, thumbNail);
+            }
 		}
 		return thumbNail;
+	}
+
+	private byte[] searchForOrCreateThumbnail(File jpegFile) throws IOException {
+	    byte[] thumbNailData = null;
+
+	    logger.logDEBUG("Searching for thumbnail in image: "  + jpegFile.getAbsolutePath());
+	    thumbNailData = JPEGUtil.searchForThumbnail(jpegFile);
+
+	    if (thumbNailData != null) {
+	        logger.logDEBUG("Found thumbnail in image: "  + jpegFile.getAbsolutePath());
+	        return thumbNailData;
+	    } else {
+	        logger.logDEBUG("Creating thumbnail for image: "  + jpegFile.getAbsolutePath());
+
+	        String algorithm = config.getStringProperty("thumbnails.view.create.algorithm");
+	        logger.logDEBUG("Thumbnail creation algorithm: " + algorithm);
+
+	        return thumbNailData = JPEGUtil.createThumbNail(jpegFile, config.getIntProperty("thumbnails.view.width"), config.getIntProperty("thumbnails.view.height"), algorithm.equals("FAST") ? JPEGScaleAlgorithm.FAST : JPEGScaleAlgorithm.SMOOTH);
+	    }
 	}
 }
