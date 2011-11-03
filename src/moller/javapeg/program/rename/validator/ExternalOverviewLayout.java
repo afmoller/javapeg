3fee64258b7fbe5843fe473bb01fdc87f9403162
fredrik_m <afmoller@users.noreply.github.com>
2011-11-03 20:16:00 +0000

    White spaces removed.

--- src/moller/javapeg/program/rename/validator/ExternalOverviewLayout.java
+++ src/moller/javapeg/program/rename/validator/ExternalOverviewLayout.java
@@ -1,11 +1,4 @@
 package moller.javapeg.program.rename.validator;
-/**
- * This class was created : 2009-05-08 by Fredrik Möller
- * Latest changed         : 2009-05-09 by Fredrik Möller
- *                        : 2009-05-10 by Fredrik Möller
- *                        : 2009-05-11 by Fredrik Möller
- *                        : 2009-08-21 by Fredrik Möller
- */
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,17 +11,17 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
-import org.xml.sax.SAXException;
-
 import moller.javapeg.StartJavaPEG;
 import moller.javapeg.program.language.Language;
 import moller.javapeg.program.logger.Logger;
 import moller.javapeg.program.rename.ValidatorStatus;
 
+import org.xml.sax.SAXException;
+
 /**
- * This class validates the content of an external layout.xml file, if it 
- * exists against a schema. 
- * 
+ * This class validates the content of an external layout.xml file, if it
+ * exists against a schema.
+ *
  * @author Fredrik
  */
 
@@ -43,7 +36,7 @@ public class ExternalOverviewLayout {
 	 * The system dependent file separator char
 	 */
 	private final static String FS = File.separator;
-	
+
 	/**
 	 * Private constructor.
 	 */
@@ -52,7 +45,7 @@ public class ExternalOverviewLayout {
 
 	/**
 	 * Accessor method for this Singleton class.
-	 * 
+	 *
 	 * @return the singleton instance of this class.
 	 */
 	public static ExternalOverviewLayout getInstance() {
@@ -65,41 +58,41 @@ public class ExternalOverviewLayout {
 			return instance;
 		}
 	}
-		
+
 	/**
-	 * This method will validate an external layout.xml files, which is used to 
+	 * This method will validate an external layout.xml files, which is used to
 	 * define the layout of the thumb nail overview, if it exists.
-	 * 
+	 *
 	 * If there is no external layout.xml file this method returns with status
 	 * equal to success, otherwise, the validation starts and the return value
-	 * depends on whether the validation against the schema layout.xsd was valid 
+	 * depends on whether the validation against the schema layout.xsd was valid
 	 * or not
-	 *  
-	 * @return a {@link ValidatorStatus} object which values depends on the 
+	 *
+	 * @return a {@link ValidatorStatus} object which values depends on the
 	 *         validation result. If the validation was successful the values
 	 *         are true and an empty message string otherwise the values are
-	 *         false and a message specifying the validation error. 
+	 *         false and a message specifying the validation error.
 	 */
 	public ValidatorStatus test() {
-		
+
 		Logger log = Logger.getInstance();
 		Language lang = Language.getInstance();
-		
+
 		ValidatorStatus vs = new ValidatorStatus(true, "");
-		
+
 		InputStream layoutFile = null;
-				
+
 		try {
 			layoutFile = new FileInputStream(System.getProperty("user.dir") + FS + "resources" + FS + "thumb" + FS + "layout.xml");
 		} catch (FileNotFoundException e) {
 			log.logDEBUG("No external layout.xml file was found in folder: " + System.getProperty("user.dir") + FS + "resources" + FS + "thumb" + FS);
 			return vs;
 		}
-		
+
 		try {
 			SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 			Schema schema = factory.newSchema(new StreamSource(StartJavaPEG.class.getResourceAsStream("resources/thumbnailoverview/layout.xsd")));
-			
+
 			Validator validator = schema.newValidator();
 			validator.validate(new StreamSource(layoutFile));
 		} catch (SAXException ex) {
@@ -109,7 +102,7 @@ public class ExternalOverviewLayout {
 			return vs;
 		} catch (Exception ex) {
 			log.logERROR(ex);
-		}		
+		}
 		return vs;
 	}
 }
