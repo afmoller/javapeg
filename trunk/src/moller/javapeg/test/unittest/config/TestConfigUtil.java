216856eef1cd3a9c2dc562ecd64b25fb556a4940
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2012-03-30 21:10:00 +0000

    Added methods to see whether the client id is set or not and a way to generate a client id.

---
+++ trunk/src/moller/javapeg/test/unittest/config/TestConfigUtil.java
@@ -0,0 +1,24 @@
+package moller.javapeg.test.unittest.config;
+
+import java.util.UUID;
+
+import junit.framework.Assert;
+import moller.javapeg.program.config.ConfigUtil;
+
+import org.junit.Test;
+
+public class TestConfigUtil {
+
+    @Test
+    public void testIsClientIdSet() {
+        Assert.assertFalse(ConfigUtil.isClientIdSet("NOT-DEFINED"));
+        Assert.assertTrue(ConfigUtil.isClientIdSet(UUID.randomUUID().toString()));
+    }
+
+    @Test
+    public void testGenerateClientId() {
+        String clientId = ConfigUtil.generateClientId();
+
+        Assert.assertTrue(clientId.matches("[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}"));
+    }
+}
