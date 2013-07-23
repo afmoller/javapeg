b04e7e6b55089ab6a21c82b9b688d26169e0d65c
fredrik_m <afmoller@users.noreply.github.com>
2013-07-23 20:14:52 +0000

    Fixed a problem with the adding of children to an empty categories tree did not show up.

--- src/moller/util/gui/TreeUtil.java
+++ src/moller/util/gui/TreeUtil.java
@@ -17,50 +17,50 @@ public class TreeUtil {
 
 	@SuppressWarnings("unchecked")
 	public static void expandEntireTree(JTree tree, DefaultMutableTreeNode root, boolean expandRoot) {
-		
+
 		if (expandRoot) {
 			tree.expandPath(new TreePath(root.getPath()));
 		}
-		
+
 		Enumeration<DefaultMutableTreeNode> children = root.children();
-		
+
 		while (children.hasMoreElements()) {
 			DefaultMutableTreeNode child = children.nextElement();
-			
+
 			tree.expandPath(new TreePath(child.getPath()));
-			
+
 			if(child.children().hasMoreElements()) {
 				expandEntireTree(tree, child, expandRoot);
 			}
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static void collapseEntireTree(JTree tree, DefaultMutableTreeNode root, boolean collapseRoot) {
-		
+
 		Enumeration<DefaultMutableTreeNode> children = root.children();
-		
+
 		while (children.hasMoreElements()) {
 			DefaultMutableTreeNode child = children.nextElement();
-			
+
 			if(child.children().hasMoreElements()) {
 				collapseEntireTree(tree, child, collapseRoot);
 			}
 			tree.collapsePath(new TreePath(child.getPath()));
 		}
-		
+
 		if (collapseRoot) {
 			tree.collapsePath(new TreePath(root.getPath()));
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static void insertNodeInAlphabeticalOrder(DefaultMutableTreeNode childNode, DefaultMutableTreeNode parentNode, DefaultTreeModel model) {
 		Enumeration<DefaultMutableTreeNode> children = parentNode.children();
-		
+
 		String nodeName = childNode.toString();
 		int index = 0;
-		
+
 		if (children.hasMoreElements()) {
 			while (children.hasMoreElements()) {
 				String displayString = children.nextElement().toString();
@@ -74,33 +74,33 @@ public class TreeUtil {
 			model.insertNodeInto(childNode, parentNode, 0);
 		}
 	}
-	
+
 	public static void sortNodesAlphabetically(DefaultMutableTreeNode nodeToSort, DefaultTreeModel model) {
 		int nrOfChildren = nodeToSort.getChildCount();
-		
+
 		if (nrOfChildren > 1) {
 			Map<String, DefaultMutableTreeNode> nameNodeMap = new HashMap<String, DefaultMutableTreeNode>(nrOfChildren);
-			
+
 			for (int i = 0; i < nrOfChildren; i++) {
 				DefaultMutableTreeNode child = (DefaultMutableTreeNode)nodeToSort.getChildAt(i);
 				nameNodeMap.put(child.toString(), child);
 			}
 
 			List<String> names = new ArrayList<String>(nameNodeMap.keySet());
-			
+
 			Collections.sort(names, new IgnoreCaseComparator());
-			
+
 			for (DefaultMutableTreeNode node : nameNodeMap.values()) {
-				model.removeNodeFromParent(node);	
+				model.removeNodeFromParent(node);
 			}
-			
+
 			for (int i = 0; i < names.size(); i++) {
 				model.insertNodeInto(nameNodeMap.get(names.get(i)), nodeToSort, i);
 			}
 			model.reload();
 		}
 	}
-	
+
 	private static class IgnoreCaseComparator implements Comparator<String> {
 		public int compare(String o1, String o2) {
 			return o1.compareToIgnoreCase(o2);
