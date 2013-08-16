ef81e123d2872fbd731a2eadd9526fe24ede973d
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2013-08-16 20:43:13 +0000

    Started to implement an image resize functionality.

---
+++ trunk/src/moller/javapeg/program/enumerations/CategoryMenueType.java
@@ -0,0 +1,12 @@
+package moller.javapeg.program.enumerations;
+
+public enum CategoryMenueType {
+    ALL,
+    NO_COLLAPSE,
+    NO_EXPAND,
+    NO_EXPAND_COLLAPSE,
+    NO_RENAME_REMOVE,
+    NO_RENAME_REMOVE_COLLAPSE,
+    NO_RENAME_REMOVE_EXPAND_COLLAPSE,
+    NO_RENAME_REMOVE_EXPAND;
+}
