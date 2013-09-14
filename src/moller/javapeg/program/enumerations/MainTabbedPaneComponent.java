4b3f53a6b26c94bb398ce15055093be1caf88889
fredrik_m <afmoller@users.noreply.github.com>
2013-09-14 19:14:30 +0000

    Started to implement an directory merge functionality.

--- src/moller/javapeg/program/enumerations/MainTabbedPaneComponent.java
+++ src/moller/javapeg/program/enumerations/MainTabbedPaneComponent.java
@@ -1,7 +1,16 @@
 package moller.javapeg.program.enumerations;
 
+/**
+ * This enumeration is used to set a name to the components in the tabs in the
+ * main GUI window and to be able to know which tab that is currently selected
+ * by asking the component about it´s name.
+ *
+ * @author Fredrik
+ *
+ */
 public enum MainTabbedPaneComponent {
 
+    MERGE("MERGE"),
     RENAME("RENAME"),
     VIEW("VIEW"),
     CATEGORIZE("CATEGORIZE");
