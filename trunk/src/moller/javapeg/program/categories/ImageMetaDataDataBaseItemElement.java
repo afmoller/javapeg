ae65aec24e9e83e8e582c924c3c87e458af47d08
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2013-12-29 21:46:03 +0000

    Refactor to clean up code and make the flow clearer and a start to a validation step in the deserialization of the image meta data data base XML files.

---
+++ trunk/src/moller/javapeg/program/categories/ImageMetaDataDataBaseItemElement.java
@@ -0,0 +1,20 @@
+package moller.javapeg.program.categories;
+
+public class ImageMetaDataDataBaseItemElement {
+
+    public static final String FILE = "file";
+    public static final String IMAGE = "image";
+    public static final String JAVAPEG_ID = "javapeg-id";
+    public static final String CATEGORIES = "categories";
+    public static final String RATING = "rating";
+    public static final String COMMENT = "comment";
+    public static final String EXPOSURE_TIME = "exposure-time";
+    public static final String PICTURE_WIDTH = "picture-width";
+    public static final String PICTURE_HEIGHT = "picture-height";
+    public static final String ISO_VALUE = "iso-value";
+    public static final String DATE_TIME = "date-time";
+    public static final String CAMERA_MODEL = "camera-model";
+    public static final String F_NUMBER = "f-number";
+    public static final String EXIF_META_DATA = "exif-meta-data";
+
+}
