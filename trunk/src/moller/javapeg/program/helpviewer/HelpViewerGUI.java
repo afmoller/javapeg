5e0ff8a8a812834ffec298705fae80f159d6abc7
fredrik_m <fredrik_m@2af1ebec-2c7e-493c-8ea7-1e2665debaa5>
2015-01-14 20:50:53 +0000

    The help texts are now in html format.

--- trunk/src/moller/javapeg/program/helpviewer/HelpViewerGUI.java
+++ trunk/src/moller/javapeg/program/helpviewer/HelpViewerGUI.java
@@ -17,106 +17,64 @@
 package moller.javapeg.program.helpviewer;
 
 import java.awt.BorderLayout;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.JEditorPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
-import javax.swing.JTextArea;
 import javax.swing.JTree;
-import javax.swing.UIManager;
-import javax.swing.WindowConstants;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 import moller.javapeg.StartJavaPEG;
 import moller.javapeg.program.C;
-import moller.javapeg.program.config.Config;
-import moller.javapeg.program.config.model.Configuration;
-import moller.javapeg.program.config.model.GUI.GUI;
+import moller.javapeg.program.config.model.GUI.GUIWindow;
 import moller.javapeg.program.gui.CustomizedJScrollPane;
 import moller.javapeg.program.gui.GUIDefaults;
+import moller.javapeg.program.gui.frames.base.JavaPEGBaseFrame;
 import moller.javapeg.program.gui.icons.IconLoader;
 import moller.javapeg.program.gui.icons.Icons;
-import moller.javapeg.program.language.Language;
-import moller.javapeg.program.logger.Logger;
-import moller.util.gui.Screen;
 import moller.util.io.StreamUtil;
-import moller.util.string.StringUtil;
 
-public class HelpViewerGUI extends JFrame {
+/**
+ * <p>
+ * This class is respsonsible for displaying the localized help file contents
+ * which are available in JavaPEG.
+ * </p>
+ * <p>
+ * The expected input is html.
+ * </p>
+ *
+ * @author Fredrik
+ *
+ */
+public class HelpViewerGUI extends JavaPEGBaseFrame {
 
     private static final long serialVersionUID = 1L;
 
     private JTree tree;
 
-    private JPanel contentJPanel;
+    private JEditorPane contentJEditorPane;
     private JPanel backgroundsPanel;
 
     private JSplitPane splitPane;
 
-    private final Configuration configuration;
-    private final Logger   logger;
-    private final Language lang;
-
-    private String content = "";
-
     public HelpViewerGUI() {
-        configuration = Config.getInstance().get();
-        logger = Logger.getInstance();
-        lang   = Language.getInstance();
-
         this.initiateWindow();
         this.addListeners();
     }
 
     private void initiateWindow() {
+        loadAndApplyGUISettings();
 
-        GUI gUI = configuration.getgUI();
-
-        Rectangle sizeAndLocation = gUI.getHelpViewer().getSizeAndLocation();
-
-        this.setSize(sizeAndLocation.getSize());
-
-        Point xyFromConfig = new Point(sizeAndLocation.getLocation());
-
-        if (Screen.isVisibleOnScreen(sizeAndLocation)) {
-            this.setLocation(xyFromConfig);
-            this.setSize(sizeAndLocation.getSize());
-        } else {
-            JOptionPane.showMessageDialog(null, lang.get("helpViewerGUI.window.locationError"), lang.get("errormessage.maingui.errorMessageLabel"), JOptionPane.ERROR_MESSAGE);
-            logger.logERROR("Could not set location of Help Viewer GUI to: x = " + xyFromConfig.x + " and y = " + xyFromConfig.y + " since that is outside of available screen size.");
-
-            this.setLocation(0,0);
-            this.setSize(GUIDefaults.HELP_VIEWER_WIDTH, GUIDefaults.HELP_VIEWER_HEIGHT);
-        }
-
-        this.setLayout(new BorderLayout());
-
-        try{
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e){
-            logger.logERROR("Could not set desired Look And Feel for Help Viewer GUI");
-            logger.logERROR("Below is the generated StackTrace");
-
-            for(StackTraceElement element : e.getStackTrace()) {
-                logger.logERROR(element.toString());
-            }
-        }
         this.getContentPane().add(this.initiateSplitPane(), BorderLayout.CENTER);
 
         this.setIconImage(IconLoader.getIcon(Icons.HELP).getImage());
-        this.setTitle(lang.get("helpViewerGUI.window.title"));
+        this.setTitle(getLang().get("helpViewerGUI.window.title"));
     }
 
     private JSplitPane initiateSplitPane() {
@@ -132,28 +90,6 @@ public class HelpViewerGUI extends JFrame {
         return splitPane;
     }
 
-    private void addListeners(){
-        this.addWindowListener(new WindowEventHandler());
-        backgroundsPanel.addComponentListener(new WindowComponentListener());
-    }
-
-    private class WindowEventHandler extends WindowAdapter {
-        @Override
-        public void windowClosing(WindowEvent e) {
-            updateWindowLocationAndSize();
-            setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        }
-    }
-
-    private void updateWindowLocationAndSize() {
-        GUI gUI = configuration.getgUI();
-
-        Rectangle sizeAndLocation = gUI.getHelpViewer().getSizeAndLocation();
-
-        sizeAndLocation.setLocation(this.getLocation().x, this.getLocation().y);
-        sizeAndLocation.setSize(this.getSize().width, this.getSize().height);
-    }
-
     private JScrollPane initiateJTree() {
         tree = new JTree(HelpViewerGUIUtil.createNodes());
         tree.setShowsRootHandles(true);
@@ -162,34 +98,27 @@ public class HelpViewerGUI extends JFrame {
     }
 
     private CustomizedJScrollPane initiateContentPanel() {
-        contentJPanel = new JPanel(new BorderLayout());
-        return new CustomizedJScrollPane(contentJPanel);
+        contentJEditorPane = new JEditorPane();
+        contentJEditorPane.setEditable(false);
+        contentJEditorPane.setContentType("text/html");
+        return new CustomizedJScrollPane(contentJEditorPane);
     }
 
-    private JTextArea getContent(String identityString) {
-        if (content.equals("") && identityString != null) {
+    private String getHelpFileContent(String identityString) {
+        String helpFileContent = "";
 
-            String confLang = configuration.getLanguage().getgUILanguageISO6391();
-
-            InputStream helpFile = null;
+        if (identityString != null) {
+            String confLang = getConfiguration().getLanguage().getgUILanguageISO6391();
 
             try {
-                helpFile = StartJavaPEG.class.getResourceAsStream("resources/help/" + confLang + "/" + identityString);
-                content = StreamUtil.getString(helpFile, C.UTF8);
+                InputStream helpFile = StartJavaPEG.class.getResourceAsStream("resources/help/" + confLang + "/" + identityString);
+                helpFileContent = StreamUtil.getString(helpFile, C.UTF8);
             } catch (IOException iox) {
-                logger.logFATAL("Could not read content from helpfile");
-                logger.logFATAL(iox);
+                getLogger().logFATAL("Could not read content from helpfile");
+                getLogger().logFATAL(iox);
             }
         }
-
-        JTextArea textarea = new JTextArea();
-        textarea.setLineWrap(true);
-        textarea.setWrapStyleWord(true);
-        textarea.setEditable(false);
-        textarea.setText(StringUtil.formatString(content));
-        textarea.setCaretPosition(0);
-
-        return textarea;
+        return helpFileContent;
     }
 
     /**
@@ -203,20 +132,21 @@ public class HelpViewerGUI extends JFrame {
             if(selRow > -1) {
                 String identity = ((UserObject)((DefaultMutableTreeNode)tree.getLastSelectedPathComponent()).getUserObject()).getIdentityString();
 
-                content = "";
-                contentJPanel.removeAll();
-                contentJPanel.updateUI();
-                contentJPanel.add(getContent(identity), BorderLayout.CENTER);
+                contentJEditorPane.removeAll();
+                contentJEditorPane.updateUI();
+                contentJEditorPane.setText(getHelpFileContent(identity));
+                contentJEditorPane.setCaretPosition(0);
             }
         }
     }
 
-    private class WindowComponentListener extends ComponentAdapter {
-        @Override
-        public void componentResized(ComponentEvent e) {
-            contentJPanel.removeAll();
-            contentJPanel.updateUI();
-            contentJPanel.add(getContent(null), BorderLayout.CENTER);
-        }
+    @Override
+    public GUIWindow getGUIWindowConfig() {
+        return getConfiguration().getgUI().getHelpViewer();
+    }
+
+    @Override
+    public Dimension getDefaultSize() {
+        return new Dimension(GUIDefaults.HELP_VIEWER_WIDTH, GUIDefaults.HELP_VIEWER_HEIGHT);
     }
 }
